0c11d198fb65845aafa98617cbe160633d97aa42
Ehud Ben-Reuven <udi.benreuven@gmail.com>
2018-04-10 08:49:32 -0400

    fix run_server not to hangup on stderr

--- run-server.bash
+++ run-server.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
-ganache-cli -a 100 > log &
+# we dont want to be deterministic so dont use -d or -s or -m
+# this will make sure browsers running older versions will not interact
+# start with 100 accounts
+ganache-cli -a 100 2>&1 1>log &
 ipfs daemon 2>&1 1>/dev/null &
 truffle migrate
 npm run dev 2>&1 1>/dev/null &
-
