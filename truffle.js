42bcacf83743a699533dfea67975025702ad8249
Ehud Ben-Reuven <udi.benreuven@gmail.com>
2018-04-23 06:11:34 -0400

    merege improvements to local from signed-link

--- truffle.js
+++ truffle.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 8545,
+      port: 8877,
       network_id: '*', // Match any network id
       gas: 3141500
     }
