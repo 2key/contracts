3e0a6cbdbfb3bcffd6fd0e26a0d943746f2c9441
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-07-16 11:24:31 +0300

    #311 Remove husky

--- SOLInterfaceHelpers.js
+++ SOLInterfaceHelpers.js
@@ -6,23 +6,10 @@ const simpleGit = require('simple-git')
 
 const readdir = util.promisify(fs.readdir);
 const buildPath = path.join(__dirname, 'build', 'contracts');
-const abiPath = path.join(__dirname, 'sol-interface');
+const abiPath = path.join(__dirname, 'build', 'sol-interface');
 const actions = {};
 
-// console.log(process.env);
-// process.env.GIT_DIR = abiPath;
-const git = simpleGit(abiPath);
-// const git = simpleGit(process.env.GIT_DIR.replace('/.git', ''));
-
-const getStatus = new Promise((resolve, reject) => {
-  git.status((err, status) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(status);
-    }
-  });
-});
+let git = simpleGit();
 
 const setAction = {
   generate: () => { actions.doGenerate = true },
@@ -37,7 +24,63 @@ process.argv.forEach(arg => {
   }
 });
 
-console.log('actions', actions);
+const gitHelpers = {
+  fetch() {
+    return new Promise((resolve, reject) => {
+      git.fetch((err, status) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(status);
+        }
+      });
+    });
+  },
+  status() {
+    return new Promise((resolve, reject) => {
+      git.status((err, status) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(status);
+        }
+      });
+    });
+  },
+  clone() {
+    return new Promise((resolve, reject) => {
+      git.clone('git@github.com:2key/sol-interface.git', abiPath, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
+  branch() {
+    return new Promise((resolve, reject) => {
+      git.branch((err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
+  submoduleUpdate() {
+    return new Promise((resolve, reject) => {
+      git.submoduleUpdate((err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+};
 
 const generateSOLInterface = () => {
   if (fs.existsSync(buildPath)) {
@@ -68,46 +111,65 @@ const generateSOLInterface = () => {
 }
 
 const commitSOLInterface = () => {
-  getStatus.then(changes => {
-    if (changes.files && changes.files.length) {
-      console.log(changes);
-      const mainCommitMsg = fs.readFileSync(path.join(__dirname, '.git/COMMIT_EDITMSG'), { encoding: 'utf-8'});
-      console.log('Message', mainCommitMsg);
-      git.add(changes.files.map(file => path.join(abiPath, file.path)), (err, result) => {
-        if (err) {
-          console.warn('Add failed:', err);
-          process.exit(1);
-        } else {
-          console.log(result);
-          git.commit(mainCommitMsg, (commitErr, commitResult) => {
-            if (commitErr) {
-              console.warn('Commit failed:', commitErr);
-              process.exit(1);
-            } else {
-              console.log(`${mainCommitMsg}commited`);
-            }
-          });
-        }
-      });
-    } else {
-      console.log('Nothing changed');
-    }
-  })
-  .catch(err => {
-    console.warn('GIT STATUS ERROR:', err);
+  gitHelpers.status()
+    .then(changes => {
+      if (changes.files && changes.files.length) {
+        console.log(changes);
+        const mainCommitMsg = fs.readFileSync(path.join(__dirname, '.git/COMMIT_EDITMSG'), { encoding: 'utf-8' });
+        console.log('Message', mainCommitMsg);
+        git.add(changes.files.map(file => path.join(abiPath, file.path)), (err, result) => {
+          if (err) {
+            console.warn('Add failed:', err);
+            process.exit(1);
+          } else {
+            console.log(result);
+            git.commit(mainCommitMsg, (commitErr, commitResult) => {
+              if (commitErr) {
+                console.warn('Commit failed:', commitErr);
+                process.exit(1);
+              } else {
+                console.log(`${mainCommitMsg}commited`);
+              }
+            });
+          }
+        });
+      } else {
+        console.log('Nothing changed');
+      }
+    })
+    .catch(err => {
+      console.warn('GIT STATUS ERROR:', err);
+      process.exit(1);
+    });
+}
+
+async function main() {
+  await gitHelpers.fetch();
+  const mainStatus = await gitHelpers.status();
+  console.log(mainStatus);
+  if (mainStatus.behind || mainStatus.files.length) {
+    console.log('You have unsynced changes!');
     process.exit(1);
-  });
+  }
+  // git = simpleGit(abiPath);
+  // console.log(mainBranches);
+  // console.log('actions', actions);
+  // const solBranches = await gitHelpers.branch();
+  // console.log(solBranches);
+  // const brachExist = solBranches.all.filter(item => item.includes(mainBranches.current));
+
+  if (actions.doGenerate) {
+    generateSOLInterface();
+  } else if (actions.doCommit) {
+    commitSOLInterface();
+  } else if (actions.doTag) {
+
+  } else if (actions.doPush) {
+
+  } else {
+    console.log('Not enough arguments!');
+    process.exit(1);
+  }
 }
 
-if (actions.doGenerate) {
-  generateSOLInterface();
-} else if (actions.doCommit) {
-  commitSOLInterface();
-} else if (actions.doTag) {
-
-} else if (actions.doPush) {
-
-} else {
-  console.log('Not enough arguments!');
-  process.exit(1);
-}
+main();
