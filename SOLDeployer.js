2125fbeccfee78947af169df1f9fa5463c17049e
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-07-25 14:21:51 +0300

    Remove removing artifatcs on run

--- SOLDeployer.js
+++ SOLDeployer.js
@@ -5,7 +5,7 @@ const compressor = require('node-minify');
 const simpleGit = require('simple-git/promise');
 const childProcess = require('child_process');
 const moment = require('moment');
-const rimraf = require('rimraf');
+// const rimraf = require('rimraf');
 
 const readdir = util.promisify(fs.readdir);
 const exec = util.promisify(childProcess.exec);
@@ -102,7 +102,8 @@ async function main() {
     unlinkTruffleConfig();
     console.log(process.argv);
     const truffleConfig = fs.readFileSync(truffleTemplatePath);
-    rimraf.sync(buildPath);
+    // Need review this
+    // rimraf.sync(buildPath);
     fs.writeFileSync(truffleConfigPath, truffleConfig);
     console.time('truffle migrate');
     const truffle = childProcess.spawn('node_modules/.bin/truffle', process.argv.slice(2));
