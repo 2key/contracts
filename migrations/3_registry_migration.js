d991e772d9f44f88875b009eebf409b68a4812df
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-01 08:39:45 +0300

    Add TwoKeyContract.sol

--- migrations/3_registry_migration.js
+++ migrations/3_registry_migration.js
@@ -1,4 +1,5 @@
 const Registry = artifacts.require('TwoKeyReg');
+const Campaign = artifacts.require('TwoKeyContract');
 
 module.exports = function(deployer, network, accounts) {
   if (deployer.network == "development" || deployer.network == "rinkeby-infura") {
