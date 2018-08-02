fa2bc7747128f6c54779bbfebd29448f59d3e2c5
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-02 16:16:34 +0300

    Add TwoKeySignedPresellContract to migration

--- migrations/3_registry_migration.js
+++ migrations/3_registry_migration.js
@@ -1,4 +1,5 @@
 const Registry = artifacts.require('TwoKeyReg');
+const TwoKeySignedPresellContract = artifacts.require('TwoKeySignedPresellContract');
 
 module.exports = function(deployer, network, accounts) {
   if (deployer.network == "development" || deployer.network == "rinkeby-infura") {
