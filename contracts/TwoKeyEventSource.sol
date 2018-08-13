a3dd15cfe0650a4653d55e654aad80c41431eadd
madjarevicn <nikola.madjarevic@gmail.com>
2018-08-13 18:11:34 +0200

    Implement roles

--- contracts/TwoKeyEventSource.sol
+++ contracts/TwoKeyEventSource.sol
@@ -1,14 +1,27 @@
 pragma solidity ^0.4.24;
 
 import './TwoKeyTypes.sol';
+import "./TwoKeyAdmin.sol";
 
 contract TwoKeyEventSource is TwoKeyTypes {
+
+    /// Events
+    event Created(address indexed _campaign, address indexed _owner);
+    event Joined(address indexed _campaign, address indexed _from, address indexed _to);
+    event Escrow(address indexed _campaign, address indexed _escrow, address indexed _sender, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type);
+    event Rewarded(address indexed _campaign, address indexed _to, uint256 _amount);
+    event Fulfilled(address indexed _campaign, address indexed _converter, uint256 indexed _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type);
+    event Cancelled(address indexed _escrow, address indexed _converter, uint256 indexed _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type);
+
+
     ///Address of the contract admin
     TwoKeyAdmin twoKeyAdmin;
 
     ///Mapping contract bytecode to boolean if is allowed to emit an event
     mapping(bytes32 => bool) canEmit;
 
+    ///Mapping an address to boolean if allowed to modify
+    mapping(address => bool) allowedModifiers;
 
     ///@notice Modifier which allows only admin to call a function - can be easily modified if there is going to be more admins
     modifier onlyAdmin {
@@ -16,6 +29,11 @@ contract TwoKeyEventSource is TwoKeyTypes {
         _;
     }
 
+    ///@notice Modifier which allows all modifiers to update canEmit mapping - ever
+    modifier onlyAllowedModifiers {
+        require(allowedModifiers(msg.sender) == true || msg.sender == address(twoKeyAdmin));
+        _;
+    }
 
     ///@notice Modifier which will only allow allowed contracts to emit an event
     modifier onlyAllowedContracts {
@@ -28,7 +46,7 @@ contract TwoKeyEventSource is TwoKeyTypes {
     /// @param _contractAddress is actually the address of contract we'd like to allow
     /// @dev We first fetch bytes32 contract code and then update our mapping
     /// @dev only admin can call this or an authorized person
-    function addContract(address _contractAddress) public onlyAdmin {
+    function addContract(address _contractAddress) public onlyAllowedModifiers {
         bytes32 _contractCode;
         canEmit[_contractCode] = true;
     }
@@ -37,43 +55,61 @@ contract TwoKeyEventSource is TwoKeyTypes {
     /// @param _contractAddress is actually the address of contract we'd like to disable
     /// @dev We first fetch bytes32 contract code and then update our mapping
     /// @dev only admin can call this or an authorized person
-    function removeContract(address _contractAddress) public onlyAdmin {
+    function removeContract(address _contractAddress) public onlyAllowedModifiers {
         bytes32 _contractCode;
         canEmit[_contractCode] = false;
     }
 
+    /// @notice Function where an admin can authorize any other person to modify allowed contracts
+    /// @param _newModifier is the address of new modifier contract / account
+    /// @dev if only contract can be modifier then we'll add one more validation step
+    function addModifier(address _newModifier) public onlyAdmin {
+        require(_newModifier != address(0));
+        allowedModifiers[_newModifier] = true;
+    }
+
+    /// @notice Function to remove authorization from an modifier
+    /// @param _oldModifier is the address of modifier
+    /// @dev checking if that address is set to true before since we'll spend 21k gas if it's already false to override that value
+    function removeModifier(address _oldModifier) public onlyAdmin {
+        require(_oldModifier != address(0));
+        require(allowedModifiers[_oldModifier] == true);
+
+        allowedModifiers[_oldModifier] = false;
+    }
+    /// @notice Constructor during deployment of contract we need to set an admin address (means TwoKeyAdmin needs to be previously deployed)
+    /// @param _twoKeyAdminAddress is the address of TwoKeyAdmin contract previously deployed
     constructor(address _twoKeyAdminAddress) public {
         twoKeyAdmin = TwoKeyAdmin(_twoKeyAdminAddress);
     }
 
-    event Created(address indexed _campaign, address indexed _owner);
-    event Joined(address indexed _campaign, address indexed _from, address indexed _to);
-	event Escrow(address indexed _campaign, address indexed _escrow, address indexed _sender, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type);
-	event Rewarded(address indexed _campaign, address indexed _to, uint256 _amount);
-    event Fulfilled(address indexed _campaign, address indexed _converter, uint256 indexed _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type);
-    event Cancelled(address indexed _escrow, address indexed _converter, uint256 indexed _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type);
-
-    function created(address _campaign, address _owner) public {
+    /// @dev Only allowed contracts can call this function ---> means can emit events
+    function created(address _campaign, address _owner) public onlyAllowedContracts{
     	emit Created(_campaign, _owner);
     }
 
-    function joined(address _campaign, address _from, address _to) public {
+    /// @dev Only allowed contracts can call this function ---> means can emit events
+    function joined(address _campaign, address _from, address _to) public onlyAllowedContracts{
     	emit Joined(_campaign, _from, _to);
     }
 
-    function escrow(address _campaign, address _escrow, address _sender, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type) public {
+    /// @dev Only allowed contracts can call this function ---> means can emit events
+    function escrow(address _campaign, address _escrow, address _sender, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type) public onlyAllowedContracts{
     	emit Escrow(_campaign, _escrow, _sender, _tokenID, _childContractID, _indexOrAmount, _type);
     }
 
-    function rewarded(address _campaign, address _to, uint256 _amount) public {
+    /// @dev Only allowed contracts can call this function ---> means can emit events
+    function rewarded(address _campaign, address _to, uint256 _amount) public onlyAllowedContracts {
     	emit Rewarded(_campaign, _to, _amount);
 	}
 
-	function fulfilled(address  _campaign, address _converter, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type) public {
+    /// @dev Only allowed contracts can call this function ---> means can emit events
+	function fulfilled(address  _campaign, address _converter, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type) public onlyAllowedContracts {
 		emit Fulfilled(_campaign, _converter, _tokenID, _childContractID, _indexOrAmount, _type);
 	}
 
-	function cancelled(address  _escrow, address _converter, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type) public {
+    /// @dev Only allowed contracts can call this function ---> means can emit events
+	function cancelled(address  _escrow, address _converter, uint256 _tokenID, address _childContractID, uint256 _indexOrAmount, CampaignType _type) public onlyAllowedContracts{
 		emit Cancelled(_escrow, _converter, _tokenID, _childContractID, _indexOrAmount, _type);
 	}
 
