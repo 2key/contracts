53c55a6260ef2cfa1d47269bf197ff44fef39db0
Yoram Kornatzky <kornatzky@gmail.com>
2018-08-13 15:11:05 +0300

    Remove wrong comment from script

--- compile-with-solc.bash
+++ compile-with-solc.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-# $1 parameter is absolute pathb to repo
+
 solc --bin --abi -o ./solcoutput github.com/OpenZeppelin/openzeppelin-solidity/contracts=$PWD/node_modules/openzeppelin-solidity/contracts  ./contracts/*.sol
