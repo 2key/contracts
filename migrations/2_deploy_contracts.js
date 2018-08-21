6b6abca2fc00ba3a91751f9ca81c0129d15ff9b0
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-21 09:02:56 +0300

    #464 Change migrations add Campaign contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@
 // * sample contract for each type of campagin.
 // * Use fake constractor parameters. coinbase is the contractor
 const TwoKeyEconomy = artifacts.require('TwoKeyEconomy');
+const TwoKeyCampaignETHCrowdsale = artifacts.require('TwoKeyCampaignETHCrowdsale');
 
 module.exports = function (deployer) {
   if (deployer.network.startsWith('dev') || deployer.network == 'rinkeby-infura') {
