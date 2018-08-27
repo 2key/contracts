dd556a93b4a6839ad60eb5294f733b8ed75117e5
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-27 09:41:22 +0300

    #464 Merge from sol-deployer

--- truffle.js
+++ truffle.js
@@ -1,37 +1,99 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
-const HDWalletProvider = require('./WalletProvider');
+require('babel-register');
+// https://github.com/trufflesuite/truffle-hdwallet-provider
+const HDWalletProvider = require('truffle-hdwallet-provider');
+// const HDWalletProvider = require('./WalletProvider');
+
 const mnemonic = 'laundry version question endless august scatter desert crew memory toy attract cruel';
+// make sure you have Ether on rinkeby address 0xb3fa520368f2df7bed4df5185101f303f6c7decc
+
 
 module.exports = {
-    networks: {
-        development: {
-            host: 'localhost',
-            port: 8545,
-            network_id: '*', // Match any network id
-            gas: 5141500,
-            gasPrice: 2000000000
-        },
-        'geth-local': {
-            provider: () => new HDWalletProvider(mnemonic, 'http://localhost:8545'),
-            network_id: '*',
-            gas: 7000000,
-            gasPrice: 2000000000,
-        },
-        'rinkeby-infura': {
-            provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/6rAARDbMXpJlwODa2kbk'),
-            network_id: '*',
-            gas: 7000000,
-            gasPrice: 2000000000,
-        },
-
-        'kovan-infura': {
-            provider: function() {
-                return new HDWalletProvider(mnemonic, `https://kovan.infura.io/6rAARDbMXpJlwODa2kbk`);
-            },
-            network_id: '42',
-            gasPrice: 2000000000
-        }
-    }
-}
-
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8500,
+      // provider: new HDWalletProvider(mnemonic, "http://localhost:8500"),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'dev-local': {
+      // host: 'localhost',
+      // port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, 'http://localhost:8545'),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'dev-ap': {
+      // host: 'localhost',
+      // port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, 'http://192.168.47.100:8545'),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 5000000000,
+    },
+    'dev-ap-ws': {
+      // host: 'localhost',
+      // port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, 'ws://192.168.47.100:8546', true),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'dev-2key': {
+      // host: 'localhost',
+      // port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, 'http://18.233.2.70:8500'),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'dev-2key-ws': {
+      // host: 'localhost',
+      // port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, 'ws://18.233.2.70:8501', true),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'geth-local': {
+      provider: () => new HDWalletProvider(mnemonic, 'http://localhost:8545'),
+      network_id: '*',
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'poc-dev': {
+      provider: () => new HDWalletProvider(mnemonic, 'http://poc-dev.2key.network:3000'),
+      network_id: '*',
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'rinkeby-infura': {
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/6rAARDbMXpJlwODa2kbk'),
+      network_id: '*',
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    'rinkeby-infura-ws': {
+      provider: () => new HDWalletProvider(mnemonic, 'wss://rinkeby.infura.io/ws', true),
+      network_id: '*',
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+    plasma: {
+      host: 'localhost',
+      port: 8888,
+      network_id: '*', // Match any network id
+    },
+    'plasma-ap': {
+      // host: 'localhost',
+      // port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, 'http://192.168.47.100:18545'),
+      network_id: '*', // Match any network id
+      gas: 7000000,
+      gasPrice: 2000000000,
+    },
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,39 @@
-const path = require('path')
-const webpack = require('webpack')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  entry: './app/javascripts/app.js',
+const libraryName = 'twokey-protocol';
+
+const config = {
+  entry: './2key-protocol/index.ts',
+  devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js'
+    path: path.join(__dirname, 'build', '2key-protocol'),
+    filename: 'index.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
-  plugins: [
-    // new HtmlWebpackPlugin({
-    //             template: 'app/index.html'
-    //         }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    // Copy our app's index.html to the dist folder.
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: 'index.html' },
-      { from: './app/images/clippy.svg', to: 'clippy.svg' }
-    ]),
-    new CleanWebpackPlugin(['dist'])
-  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        test: /(\.tsx|\.ts)$/,
+        loader: 'ts-loader',
       },
-      // {
-      //   test: /\.md$/,
-      //   use: ['html-loader', 'markdown-loader']
-      // },
-      //
-      //   {
-      //       test: /\.(html)$/,
-      //       use: {
-      //           loader: 'html-loader',
-      //       }
-      //   },
-      {
-        test: /\.(html)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].html'
-          }
-        }],
-        exclude: path.resolve(__dirname, 'index.html')
-      },
-      {
-        test: /\.(md)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].html'
-            }
-          },
-          {
-            loader: 'markdown-loader'
-          }
-        //         {
-        //             loader: 'html-loader!markdown-loader!file-loader',
-        //             options: {
-        //               name: '[name].html'
-        //             }
-        //         }
-        ]
-      }
-
     ],
-    loaders: [
-      { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      }
-    ]
   },
-  devServer: {
-    host: '0.0.0.0',
-    disableHostCheck: true
-  }
-}
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true,
+    }),
+    new ProgressBarPlugin(),
+  ],
+};
+
+module.exports = config;
