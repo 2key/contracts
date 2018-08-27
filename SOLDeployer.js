5df34e09efbf1ad5e16340e7a43bb800ba0680fa
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-27 10:28:53 +0300

    #464 Merge SOLDeployer to contracts-new

--- SOLDeployer.js
+++ SOLDeployer.js
@@ -9,24 +9,18 @@ const whitelist = require('./whitelist.json');
 
 const readdir = util.promisify(fs.readdir);
 const buildPath = path.join(__dirname, 'build', 'contracts');
-const twoKeyProtocolDir = path.join(__dirname, 'build', '2key-protocol');
-const truffleTemplatePath = path.join(__dirname, 'truffle-template.js');
-const truffleConfigPath = path.join(__dirname, 'truffle.js');
+const twoKeyProtocolDir = path.join(__dirname, '2key-protocol');
+const twoKeyProtocolLibDir = path.join(__dirname, 'build', '2key-protocol');
 
 const contractsGit = simpleGit();
-const twoKeyProtocolGit = simpleGit(twoKeyProtocolDir);
-
-const unlinkTruffleConfig = () => {
-  if (fs.existsSync(truffleConfigPath)) {
-    fs.unlinkSync(truffleConfigPath);
-  }
-};
+const twoKeyProtocolLibGit = simpleGit(twoKeyProtocolLibDir);
 
 async function handleExit(p) {
   console.log(p);
-  unlinkTruffleConfig();
-  await contractsGit.reset('hard');
-  await twoKeyProtocolGit.reset('hard');
+  if (p !== 0) {
+    await contractsGit.reset('hard');
+    await twoKeyProtocolLibGit.reset('hard');
+  }
   process.exit();
 }
 
@@ -38,11 +32,7 @@ process.on('uncaughtException', handleExit);
 
 const generateSOLInterface = () => new Promise((resolve, reject) => {
   if (fs.existsSync(buildPath)) {
-    const contracts = {
-      // version: version.join('.'),
-      // date: new Date().toDateString(),
-      // contracts: {},
-    };
+    const contracts = {};
     readdir(buildPath).then((files) => {
       try {
         files.forEach((file) => {
@@ -55,30 +45,8 @@ const generateSOLInterface = () => new Promise((resolve, reject) => {
             contracts[contractName] = whitelist[contractName].deployed
               ? { networks } : { bytecode };
           }
-          // if (abi.length) {
-          // contracts[contractName] =
-          //  { abi, networks, bytecode: Object.keys(networks).length ? undefined : bytecode }
-          // contracts[contractName] = { abi, networks, bytecode }
-          // }
         });
-        if (!fs.existsSync(twoKeyProtocolDir)) {
-          fs.mkdirSync(twoKeyProtocolDir);
-        }
-        const twoKeyProtocolSrcDir = path.join(twoKeyProtocolDir, 'src');
-        if (!fs.existsSync(twoKeyProtocolSrcDir)) {
-          fs.mkdirSync(twoKeyProtocolSrcDir);
-        }
-        fs.writeFileSync(path.join(twoKeyProtocolSrcDir, 'contracts/meta.ts'), `export default ${util.inspect(contracts, { depth: 10 })}`);
-        // fs.writeFileSync(path.join(twoKeyProtocolSrcDir, 'abi.json'), JSON.stringify(contracts));
-        // compressor.minify({
-        //   compressor: 'gcc',
-        //   input: path.join(twoKeyProtocolDir, 'index.js'),
-        //   output: path.join(twoKeyProtocolDir, 'index.js')
-        // }).then(() => {
-        //   console.log('Done');
-        //   resolve();
-        // })
-        // .catch(reject);
+        fs.writeFileSync(path.join(twoKeyProtocolDir, 'contracts/meta.ts'), `export default ${util.inspect(contracts, { depth: 10 })}`);
         console.log('Done');
         resolve();
       } catch (err) {
@@ -108,23 +76,23 @@ const runProcess = (app, args) => new Promise((resolve, reject) => {
   });
 });
 
-async function main() {
+async function deploy() {
   try {
     await contractsGit.fetch();
     await contractsGit.submoduleUpdate();
     let contractsStatus = await contractsGit.status();
-    let twoKeyProtocolStatus = await twoKeyProtocolGit.status();
+    let twoKeyProtocolStatus = await twoKeyProtocolLibGit.status();
     if (twoKeyProtocolStatus.current !== contractsStatus.current) {
-      const twoKeyProtocolBranches = await twoKeyProtocolGit.branch();
+      const twoKeyProtocolBranches = await twoKeyProtocolLibGit.branch();
       if (twoKeyProtocolBranches.all.find(item => item.includes(contractsStatus.current))) {
-        await twoKeyProtocolGit.checkout(contractsStatus.current);
+        await twoKeyProtocolLibGit.checkout(contractsStatus.current);
       } else {
-        await twoKeyProtocolGit.checkoutLocalBranch(contractsStatus.current);
+        await twoKeyProtocolLibGit.checkoutLocalBranch(contractsStatus.current);
       }
     }
     await contractsGit.submoduleUpdate();
-    await twoKeyProtocolGit.reset('hard');
-    twoKeyProtocolStatus = await twoKeyProtocolGit.status();
+    await twoKeyProtocolLibGit.reset('hard');
+    twoKeyProtocolStatus = await twoKeyProtocolLibGit.status();
     const localChanges = contractsStatus.files
       .filter(item => !(item.path.includes('build/2key-protocol')
         || (process.env.NODE_ENV === 'development' && item.path.includes(process.argv[1].split('/').pop()))));
@@ -132,45 +100,33 @@ async function main() {
       console.log('You have unsynced changes!', localChanges);
       process.exit(1);
     }
-    unlinkTruffleConfig();
     console.log(process.argv);
-    const truffleConfig = fs.readFileSync(truffleTemplatePath);
-    // Need review this
-    // rimraf.sync(buildPath);
-    fs.writeFileSync(truffleConfigPath, truffleConfig);
     const networks = process.argv[2].split(',');
-    // const truffleJobs = [];
-
-    // networks.forEach((network) => {
-    //   truffleJobs.push(
-    //  runTruffle(['migrate', '--network', network].concat(process.argv.slice(3))));
-    // });
     const l = networks.length;
     for (let i = 0; i < l; i += 1) {
       /* eslint-disable no-await-in-loop */
-      await runProcess('./node_modules/.bin/truffle', ['migrate', '--network', networks[i]].concat(process.argv.slice(3)));
+      await runProcess(path.join(__dirname, 'node_modules/.bin/truffle'), ['migrate', '--network', networks[i]].concat(process.argv.slice(3)));
       /* eslint-enable no-await-in-loop */
     }
-    await runProcess('./node_modules/.bin/typechain', ['--force', '--outDir', path.join(twoKeyProtocolDir, 'src/contracts'), `${buildPath}/*.json`]);
-    unlinkTruffleConfig();
     await generateSOLInterface();
-    await runProcess('./node_modules/.bin/webpack');
+    await runProcess(path.join(__dirname, 'node_modules/.bin/typechain'), ['--force', '--outDir', path.join(twoKeyProtocolDir, 'contracts'), `${buildPath}/*.json`]);
+    await runProcess(path.join(__dirname, 'node_modules/.bin/webpack'));
     contractsStatus = await contractsGit.status();
-    twoKeyProtocolStatus = await twoKeyProtocolGit.status();
+    twoKeyProtocolStatus = await twoKeyProtocolLibGit.status();
     const network = networks.join('/');
     const now = moment();
     const commit = `SOL Deployed to ${network} ${now.format('lll')}`;
     const tag = `${network}-${now.format('YYYYMMDDHHmmss')}`;
     console.log(commit, tag);
-    await twoKeyProtocolGit.add(twoKeyProtocolStatus.files.map(item => item.path));
-    await twoKeyProtocolGit.commit(commit);
+    await twoKeyProtocolLibGit.add(twoKeyProtocolStatus.files.map(item => item.path));
+    await twoKeyProtocolLibGit.commit(commit);
     await contractsGit.add(contractsStatus.files.map(item => item.path));
     await contractsGit.commit(commit);
-    await twoKeyProtocolGit.addTag(tag);
+    await twoKeyProtocolLibGit.addTag(tag);
     await contractsGit.addTag(tag);
-    await twoKeyProtocolGit.push('origin', contractsStatus.current);
+    await twoKeyProtocolLibGit.push('origin', contractsStatus.current);
     await contractsGit.push('origin', contractsStatus.current);
-    await twoKeyProtocolGit.pushTags('origin');
+    await twoKeyProtocolLibGit.pushTags('origin');
     await contractsGit.pushTags('origin');
   } catch (e) {
     if (e.output) {
@@ -183,6 +139,30 @@ async function main() {
       console.warn('Error', e);
     }
     await contractsGit.reset('hard');
+  }
+}
+
+async function main() {
+  const mode = process.argv[2];
+  switch (mode) {
+    case '--migrate':
+      try {
+        const networks = process.argv[3].split(',');
+        const l = networks.length;
+        for (let i = 0; i < l; i += 1) {
+          /* eslint-disable no-await-in-loop */
+          await runProcess(path.join(__dirname, 'node_modules/.bin/truffle'), ['migrate', '--network', networks[i]].concat(process.argv.slice(4)));
+          /* eslint-enable no-await-in-loop */
+        }
+        await generateSOLInterface();
+        await runProcess(path.join(__dirname, 'node_modules/.bin/typechain'), ['--force', '--outDir', path.join(twoKeyProtocolDir, 'contracts'), `${buildPath}/*.json`]);
+        process.exit(0);
+      } catch (err) {
+        process.exit(1);
+      }
+      break;
+    default:
+      deploy();
   }
 }
 
