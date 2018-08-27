915376df8df24a3a4a94ac10e6d4fef55b21f90f
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-27 14:13:50 +0300

    #464 Add TwoKeyAdmin to deployment migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,12 @@
 // * sample contract for each type of campagin.
 // * Use fake constractor parameters. coinbase is the contractor
 const TwoKeyEconomy = artifacts.require('TwoKeyEconomy');
+const TwoKeyAdmin = artifacts.require('TwoKeyAdmin');
 
 module.exports = function (deployer) {
   if (deployer.network.startsWith('dev') || deployer.network == 'rinkeby-infura') {
     deployer.deploy(TwoKeyEconomy);
+    deployer.deploy(TwoKeyAdmin);
   } else if (deployer.network.startsWith('plasma')) {
     const TwoKeyPlasmaEvents = artifacts.require('TwoKeyPlasmaEvents');
     deployer.deploy(TwoKeyPlasmaEvents);
