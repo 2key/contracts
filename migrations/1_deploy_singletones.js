7e87e8afaecb223fda57ba5ab3c2cfa17deba414
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-28 10:23:47 +0300

    #464 Change migrations to async await

--- migrations/1_deploy_singletones.js
+++ migrations/1_deploy_singletones.js
@@ -8,16 +8,12 @@ const ERC20TokenMock = artifacts.require('ERC20TokenMock');
 const TwoKeyUpgradableExchange = artifacts.require('TwoKeyUpgradableExchange');
 const TwoKeyAdmin = artifacts.require('TwoKeyAdmin');
 
-module.exports = function (deployer) {
+module.exports = async function (deployer) {
   if (deployer.network.startsWith('dev') || deployer.network == 'rinkeby-infura') {
-    deployer.deploy(TwoKeyEconomy).then((economy) => {
-      deployer.deploy(ERC20TokenMock).then((erc20) => {
-        deployer.deploy(TwoKeyUpgradableExchange, 1, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', erc20.address)
-          .then((twoKeyUpgradableExchange) => {
-            deployer.deploy(TwoKeyAdmin, economy, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', twoKeyUpgradableExchange);
-          });
-      });
-    });
+    const economy = await deployer.deploy(TwoKeyEconomy);
+    const erc20 = await deployer.deploy(ERC20TokenMock);
+    const twoKeyUpgradableExchange = await deployer.deploy(TwoKeyUpgradableExchange, 1, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', erc20.address);
+    deployer.deploy(TwoKeyAdmin, economy, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', twoKeyUpgradableExchange);
   } else if (deployer.network.startsWith('plasma')) {
     const TwoKeyPlasmaEvents = artifacts.require('TwoKeyPlasmaEvents');
     deployer.deploy(TwoKeyPlasmaEvents);
