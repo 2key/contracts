18025b104c0b027cc3fa2d77daaf4da49deb4587
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-28 15:25:26 +0300

    #454 Clean truffle migrations

--- migrations/2_deploy_singletones.js
+++ migrations/2_deploy_singletones.js
@@ -8,6 +8,7 @@ const TwoKeyEconomy = artifacts.require('TwoKeyEconomy');
 const ERC20TokenMock = artifacts.require('ERC20TokenMock');
 const TwoKeyUpgradableExchange = artifacts.require('TwoKeyUpgradableExchange');
 const TwoKeyAdmin = artifacts.require('TwoKeyAdmin');
+const EventSource = artifacts.require('TwoKeyEventSource');
 
 module.exports = function deploy(deployer) {
   if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
@@ -17,24 +18,11 @@ module.exports = function deploy(deployer) {
       .then(() => TwoKeyUpgradableExchange.deployed())
       .then(() => deployer.deploy(TwoKeyAdmin, TwoKeyEconomy.address, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', TwoKeyUpgradableExchange.address))
       .then(() => TwoKeyAdmin.deployed())
+      .then(() => deployer.deploy(EventSource, TwoKeyAdmin.address))
       .then(() => true)
       .catch((err) => {
         console.log('\x1b[31m', 'Error:', err.message, '\x1b[0m');
       });
-    // const economy = await deployer.deploy(TwoKeyEconomy);
-    // const erc20 = await deployer.deploy(ERC20TokenMock);
-    // const twoKeyUpgradableExchange = await deployer.deploy(TwoKeyUpgradableExchange, 1, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', erc20.address);
-    // console.log(twoKeyUpgradableExchange.address, economy.address);
-    // await deployer.deploy(TwoKeyAdmin, economy, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', twoKeyUpgradableExchange);
-
-    // deployer.then(async () => {
-    //   const economy = await deployer.deploy(TwoKeyEconomy);
-    //   console.log(economy.address);
-    //   // const erc20 = await deployer.deploy(ERC20TokenMock);
-    //   // const twoKeyUpgradableExchange = await deployer.deploy(TwoKeyUpgradableExchange, 1, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', erc20.address);
-    //   // console.log(twoKeyUpgradableExchange.address, economy.address);
-    //   // await deployer.deploy(TwoKeyAdmin, economy, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', twoKeyUpgradableExchange);
-    // });
   } else if (deployer.network.startsWith('plasma')) {
     const TwoKeyPlasmaEvents = artifacts.require('TwoKeyPlasmaEvents');
     deployer.deploy(TwoKeyPlasmaEvents);
--- migrations/3_registry_migration.js
+++ migrations/3_registry_migration.js
@@ -1,7 +1,8 @@
+/* global artifacts */
 const Registry = artifacts.require('TwoKeyReg');
 
-module.exports = function (deployer, network, accounts) {
-  if (deployer.network.startsWith('dev') || deployer.network == 'rinkeby-infura') {
+module.exports = function deploy(deployer, network, accounts) {
+  if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
     deployer.deploy(Registry);
   }
 };
