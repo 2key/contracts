f7ce93c49e83e37f39f8cae803a05bb91c07e5b3
madjarevicn <nikola.madjarevic@gmail.com>
2018-08-29 13:10:11 +0200

    Add interface for TwoKeyReg contract

---
+++ contracts/interfaces/ITwoKeyReg.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+import "../openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract ITwoKeyReg is Ownable{
+    function addTwoKeyEventSource(address _twoKeyEventSource) public;
+    function changeTwoKeyEventSource(address _twoKeyEventSource) public;
+    function addWhereContractor(address _userAddress, address _contractAddress) public;
+    function addWhereModerator(address _userAddress, address _contractAddress) public;
+    function addWhereRefferer(address _userAddress, address _contractAddress) public;
+    function addWhereConverter(address _userAddress, address _contractAddress) public;
+    function getContractsWhereUserIsContractor(address _userAddress) public view returns (address[]);
+    function getContractsWhereUserIsModerator(address _userAddress) public view returns (address[]);
+    function getContractsWhereUserIsRefferer(address _userAddress) public view returns (address[]);
+    function getContractsWhereUserIsConverter(address _userAddress) public view returns (address[]);
+    function getTwoKeyEventSourceAddress() public view returns (address);
+    function addNameInternal(string _name, address _sender) private;
+    function addName(string _name, address _sender) public;
+    function addNameByUser(string _name) public;
+    function getName2Owner(string _name) public view returns (address);
+    function getOwner2Name(address _sender) public view returns (string);
+}
