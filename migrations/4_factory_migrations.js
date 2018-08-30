b9bfafa5b3e833cdb878c4f0790ae7cf89fa0c5d
madjarevicn <nikola.madjarevic@gmail.com>
2018-08-30 10:59:49 +0200

    Update factory migration

--- migrations/4_factory_migrations.js
+++ migrations/4_factory_migrations.js
@@ -4,15 +4,7 @@ const EventSource = artifacts.require('TwoKeyEventSource');
 
 module.exports = function deploy(deployer) {
     if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
-
-        /*
-            Opening and closing time for ComposableAssetFactory constructor
-         */
-
-        let openingTime =  new Date().getTime() + 2000;
-        let closingTime = new Date().getTime() + 10000;
-
-        deployer.deploy(TwoKeyCampaignFactory, openingTime, closingTime)
+        deployer.deploy(TwoKeyCampaignFactory)
             .then(() => true);
     }
 };
