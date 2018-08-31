4f4ca5af3cd4166ec7a1f1b5109ddada672c6a24
madjarevicn <nikola.madjarevic@gmail.com>
2018-08-31 11:34:10 +0200

    Add migration to deploy campaign contract

---
+++ migrations/3_deploy_camapign.js
@@ -0,0 +1,21 @@
+const TwoKeyCampaign = artifacts.require('TwoKeyCampaign');
+const TwoKeyAdmin = artifacts.require('TwoKeyAdmin');
+const EventSource = artifacts.require('TwoKeyEventSource');
+const TwoKeyEconomy = artifacts.require('TwoKeyEconomy');
+const TwoKeyWhitelistedInfluencer = artifacts.require('TwoKeyWhitelisted');
+const TwoKeyWhitelistedConverter = artifacts.require('TwoKeyWhitelisted');
+const TwoKeyCampaignInventory = artifacts.require('TwoKeyCampaignInventory');
+
+
+module.exports = function deploy(deployer) {
+    if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
+        deployer.deploy(TwoKeyWhitelistedInfluencer)
+            .then(() => deployer.deploy(TwoKeyWhitelistedConverter))
+            .then(() => deployer.deploy(TwoKeyCampaignInventory))
+            .then(() => deployer.deploy(TwoKeyCampaign, EventSource.address, TwoKeyEconomy.address, TwoKeyWhitelistedInfluencer.address,
+                TwoKeyWhitelistedConverter.address, TwoKeyCampaignInventory.address,'0xb3fa520368f2df7bed4df5185101f303f6c7decc',
+                '0xb3fa520368f2df7bed4df5185101f303f6c7decc', 12345,12345,12345,12345,12345,12345))
+            .then(() => true);
+
+    }
+};
