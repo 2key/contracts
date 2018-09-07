a2d5aed1b0985164fd3f19f8a5481f45136bb11d
madjarevicn <nikola.madjarevic@gmail.com>
2018-09-07 23:16:32 +0200

    Add gas optimizations

--- migrations/3_deploy_camapign.js
+++ migrations/3_deploy_camapign.js
@@ -12,7 +12,7 @@ module.exports = function deploy(deployer) {
             .then(() => deployer.deploy(TwoKeyWhitelistedConverter))
             .then(() => deployer.deploy(TwoKeyAcquisitionCampaignERC20, EventSource.address, TwoKeyEconomy.address, TwoKeyWhitelistedInfluencer.address,
                 TwoKeyWhitelistedConverter.address,'0xb3fa520368f2df7bed4df5185101f303f6c7decc',
-                '0xb3fa520368f2df7bed4df5185101f303f6c7decc', 12345,12345,12345,12345,12345,12345))
+                '0xb3fa520368f2df7bed4df5185101f303f6c7decc', 12345,12345,12345,12345,12345,12345, {gas: 20000000}))
             .then(() => true);
     }
 };
