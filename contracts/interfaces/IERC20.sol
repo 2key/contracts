9e2ec7dab58e8b994fed0273a565e4a74b3abcc2
madjarevicn <nikola.madjarevic@gmail.com>
2018-09-11 12:02:09 +0200

    Fix

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.4.24;
 /// Interface of ERC20 contract we need in order to invoke balanceOf method from another contracts
 contract IERC20 {
     function balanceOf(address whom) view public returns (uint);
+    function transfer(address _to, uint256 _value) public returns (bool);
 }
