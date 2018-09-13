9552e6748ae0c064bafa270240e5fa7086e8dba5
madjarevicn <nikola.madjarevic@gmail.com>
2018-09-13 12:18:14 +0200

    Update & fix migration script

--- migrations/3_deploy_camapign.js
+++ migrations/3_deploy_camapign.js
@@ -7,12 +7,12 @@ const TwoKeyWhitelistedConverter = artifacts.require('TwoKeyWhitelisted');
 const TwoKeyCampaignInventory = artifacts.require('TwoKeyCampaignInventory');
 const ERC20TokenMock = artifacts.require('ERC20TokenMock');
 module.exports = function deploy(deployer) {
-    // if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
-    //     deployer.deploy(TwoKeyWhitelistedInfluencer)
-    //         .then(() => deployer.deploy(TwoKeyWhitelistedConverter))
-    //         .then(() => deployer.deploy(TwoKeyAcquisitionCampaignERC20, EventSource.address, TwoKeyEconomy.address, TwoKeyWhitelistedInfluencer.address,
-    //             TwoKeyWhitelistedConverter.address,
-    //             '0xb3fa520368f2df7bed4df5185101f303f6c7decc', 12345,12345,12345,12345,12345,12345, ERC20TokenMock.address))
-    //         .then(() => true);
-    // }
+    if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
+        deployer.deploy(TwoKeyWhitelistedInfluencer)
+            .then(() => deployer.deploy(TwoKeyWhitelistedConverter))
+            .then(() => deployer.deploy(TwoKeyAcquisitionCampaignERC20, EventSource.address, TwoKeyEconomy.address, TwoKeyWhitelistedInfluencer.address,
+                TwoKeyWhitelistedConverter.address,
+                '0xb3fa520368f2df7bed4df5185101f303f6c7decc', 12345,12345,12345,12345,12345,12345, ERC20TokenMock.address,1))
+            .then(() => true);
+    }
 };
