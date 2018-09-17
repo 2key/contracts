1b8aba0a618babf594eff902b6c25d3d8b1e496e
madjarevicn <nikola.madjarevic@gmail.com>
2018-09-17 14:36:38 +0200

    Update migrations

--- migrations/2_deploy_singletones.js
+++ migrations/2_deploy_singletones.js
@@ -42,21 +42,27 @@ const EventSource = artifacts.require('TwoKeyEventSource');
 const TwoKeyReg = artifacts.require('TwoKeyReg');
 
 module.exports = function deploy(deployer) {
-  if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
-    deployer.deploy(ERC20TokenMock)
-      .then(() => ERC20TokenMock.deployed())
-      .then(() => deployer.deploy(TwoKeyAdmin, '0xb3fa520368f2df7bed4df5185101f303f6c7decc'))
-      .then(() => TwoKeyAdmin.deployed())
-      .then(() => deployer.deploy(TwoKeyUpgradableExchange, 1, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', ERC20TokenMock.address,TwoKeyAdmin.address))
-      .then(() => TwoKeyUpgradableExchange.deployed())
-      .then(() => deployer.deploy(TwoKeyEconomy, TwoKeyAdmin.address))
-      .then(() => deployer.deploy(EventSource, TwoKeyAdmin.address))
-      .then(() => deployer.deploy(TwoKeyReg, EventSource.address, TwoKeyAdmin.address))
-      .then(() => true)
-      .catch((err) => {
-        console.log('\x1b[31m', 'Error:', err.message, '\x1b[0m');
-      });
-  } else if (deployer.network.startsWith('plasma')) {
+  var adminInstance;
+  if(deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
+    deployer.deploy(TwoKeyAdmin,'0xb3fa520368f2df7bed4df5185101f303f6c7decc')
+        .then(() => TwoKeyAdmin.deployed())
+        .then(function(instance) {
+            adminInstance = instance;
+        })
+        .then(() => console.log(adminInstance))
+        .then(() => deployer.deploy(TwoKeyEconomy, TwoKeyAdmin.address))
+        .then(() => deployer.deploy(TwoKeyUpgradableExchange, 1, '0xb3fa520368f2df7bed4df5185101f303f6c7decc', TwoKeyEconomy.address, TwoKeyAdmin.address))
+        .then(() => TwoKeyUpgradableExchange.deployed())
+        .then(() => deployer.deploy(EventSource, TwoKeyAdmin.address))
+        .then(() => EventSource.deployed())
+        .then(() => adminInstance.setTwoKeyExchange(TwoKeyUpgradableExchange.address))
+        .then(() => deployer.deploy(TwoKeyReg, EventSource.address, TwoKeyAdmin.address))
+        .then(() => TwoKeyReg.deployed())
+        .then(() => true)
+        .catch((err) => {
+            console.log('\x1b[31m', 'Error:', err.message, '\x1b[0m');
+        });
+  } else if(deployer.network.startsWith('plasma')) {
     const TwoKeyPlasmaEvents = artifacts.require('TwoKeyPlasmaEvents');
     deployer.deploy(TwoKeyPlasmaEvents);
   }
--- migrations/3_deploy_camapign.js
+++ migrations/3_deploy_camapign.js
@@ -10,6 +10,7 @@ module.exports = function deploy(deployer) {
     if (deployer.network.startsWith('dev') || deployer.network === 'rinkeby-infura') {
         deployer.deploy(TwoKeyWhitelistedInfluencer)
             .then(() => deployer.deploy(TwoKeyWhitelistedConverter))
+            .then(() => deployer.deploy(ERC20TokenMock))
             .then(() => deployer.deploy(TwoKeyAcquisitionCampaignERC20, EventSource.address, TwoKeyEconomy.address, TwoKeyWhitelistedInfluencer.address,
                 TwoKeyWhitelistedConverter.address,
                 '0xb3fa520368f2df7bed4df5185101f303f6c7decc', 12345,12345,12345,12345,12345,12345, ERC20TokenMock.address, 5))
