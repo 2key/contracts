811f051e734bfcff4ced12ead9f82ae2fd4d6cbb
madjarevicn <nikola.madjarevic@gmail.com>
2018-09-24 12:03:45 +0200

    #673 add Interface for acquisition campaign to solve stack-too-deep bug

---
+++ contracts/interfaces/ITwoKeyAcquisitionCampaignERC20.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.24;
+
+contract ITwoKeyAcquisitionCampaignERC20 {
+    function getSymbol() public view returns (string);
+    function getAssetContractAddress() public view returns (address);
+}
