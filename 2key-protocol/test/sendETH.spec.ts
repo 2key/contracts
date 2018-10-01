f4f81c023e44cf9a0fc99992f836d3f0fc6cfa30
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-10-01 10:30:10 +0300

    #464 Add sendETH test

---
+++ 2key-protocol/test/sendETH.spec.ts
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import 'mocha';
+import { TwoKeyProtocol } from '../src';
+import createWeb3 from './_web3';
+
+const { env } = process;
+
+const rpcUrl = env.RCP_URL;
+const mainNetId = env.MAIN_NET_ID;
+const syncTwoKeyNetId = env.SYNC_NET_ID;
+
+let twoKeyProtocol;
+
+const sendETH: any = (recipient) => new Promise(async (resolve, reject) => {
+    try {
+        if (!twoKeyProtocol) {
+            const {web3, address} = await createWeb3(env.MNEMONIC_DEPLOYER, rpcUrl);
+            twoKeyProtocol = new TwoKeyProtocol({
+                web3,
+                address,
+                networks: {
+                    mainNetId,
+                    syncTwoKeyNetId,
+                },
+            });
+        }
+        const txHash = await twoKeyProtocol.transferEther(recipient, twoKeyProtocol.toWei(10, 'ether'));
+        console.log(`${recipient}: ${txHash}`);
+        const receipt = await twoKeyProtocol.getTransactionReceiptMined(txHash);
+        console.log(`${receipt.status}`);
+        resolve(receipt);
+    } catch (err) {
+        reject(err);
+    }
+});
+
+describe('TwoKeyProtocol LOCAL', () => {
+    it('LOCAL: should transfer ether', async () => {
+        let error = false;
+        const addresses = Object.keys(env).filter(key => key.endsWith('_ADDRESS')).map(key => env[key]);
+        let l = addresses.length;
+        for (let i = 0; i < l; i++) {
+            const receipt = await sendETH(addresses[i]);
+            if (!receipt || receipt.status !== '0x1') {
+                error = true;
+            }
+        }
+        expect(error).to.be.false;
+    }).timeout(600000);
+});
