cba4d0e0ff1765ca395e0a9a32f5ecc3562414ed
madjarevicn <nikola.madjarevic@gmail.com>
2018-10-04 10:40:44 +0200

    Add option for contractor to change date

--- contracts/2key/TwoKeyLockupContract.sol
+++ contracts/2key/TwoKeyLockupContract.sol
@@ -7,7 +7,13 @@ contract TwoKeyLockupContract {
     uint tokens;
     address converter;
     address contractor;
+    bool changed = false;
 
+
+    modifier onlyContractor() {
+        require(msg.sender == contractor);
+        _;
+    }
     constructor(uint _tokenDistributionDate, uint _maxDistributionDateShiftInDays, uint _tokens, address _converter, address _contractor) {
         tokenDistributionDate = _tokenDistributionDate;
         maxDistributionDateShiftInDays = _maxDistributionDateShiftInDays;
@@ -15,4 +21,12 @@ contract TwoKeyLockupContract {
         converter = _converter;
         contractor = _contractor;
     }
+
+    function changeTokenDistributionDate(uint _newDate) public onlyContractor {
+        require(changed == false);
+        require(_newDate - (maxDistributionDateShiftInDays * (1days)) <= tokenDistributionDate);
+        changed = true;
+        tokenDistributionDate = _newDate;
+    }
+    //TODO: Add function to change distribution date, only once
 }
