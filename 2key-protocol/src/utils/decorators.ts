0be4056da9b4ce35c3a254b9001c66d1c5e9b1d0
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-10-09 18:25:02 +0300

    #464 2key-protocol: remove decorators

--- 2key-protocol/src/utils/decorators.ts
+++
@@ -1,9 +0,0 @@
-export function requireWeb3(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    if (!target.web3 && !(target.base && target.base.web3)) {
-        console.log(target);
-        console.log(propertyKey);
-        console.log(descriptor);
-        throw new Error('Web3 instance required');
-    }
-    return descriptor;
-}
