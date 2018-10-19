12cb5edf1bf60f7ec62e104cf7a861a4b6b8cb4e
madjarevicn <nikola.madjarevic@gmail.com>
2018-10-19 15:24:22 +0200

    Add library for bytecode generation

---
+++ solidity-bytecode-generator/generator.js
@@ -0,0 +1,59 @@
+const Web3 = require('web3');
+/*
+    @Author Nikola Madjarevic
+    Javascript library developed with goal to easily generate bytecode for any method you'd like to call
+ */
+
+/*
+    Example:
+    function doSomething(address x, string y, uint z) {}
+    methodName = "doSomething"
+    methodArgumentsTypes = [address, string, uint]
+    arguments
+*/
+
+/*
+    Will be representing of solidity method we'd like to call
+ */
+let methodName;
+
+/*
+    Will be representing all types of parameters we're passing to the method
+ */
+let methodArgumentsTypes = [];
+
+/*
+    Will be representing all concrete values of arguments
+ */
+let argumentsValues = [];
+
+
+const sig = ((methodName, methodArgumentsTypes) => {
+    let signature = methodName + "(";
+    for(let i=0; i<methodArgumentsTypes.length; i++) {
+        if(i == methodArgumentsTypes.length -1) {
+            signature += methodArgumentsTypes[i].toString()+")";
+        } else {
+            signature += methodArgumentsTypes[i].toString()+ ","
+        }
+    }
+    console.log();
+    return signature;
+});
+
+export const convertUint = ((value) => {
+    let numberOfDigits = value.toString().length;
+    let arg = "";
+    for(let i=0; i< (64- numberOfDigits); i++) {
+        arg += "0";
+    }
+    arg += value.toString();
+    return arg;
+});
+
+export const convertAddress = ((value) => {
+
+});
+
+
+console.log(sig("addValues",["address","uint","string"]));
