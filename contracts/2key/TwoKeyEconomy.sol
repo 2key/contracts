555e390768812c35ed87045686b2fe48fda2af11
madjarevicn <nikola.madjarevic@gmail.com>
2018-10-25 13:04:38 +0200

    Add option for TwoKeyAdmin to freeze - unfreeze transfers

--- contracts/2key/TwoKeyEconomy.sol
+++ contracts/2key/TwoKeyEconomy.sol
@@ -4,7 +4,6 @@ import '../openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 import '../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import '../openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import "./RBACWithAdmin.sol";
-import "./TwoKeyAdmin.sol";
 
 
 contract TwoKeyEconomy is RBACWithAdmin, StandardToken, Ownable {
@@ -20,15 +19,31 @@ contract TwoKeyEconomy is RBACWithAdmin, StandardToken, Ownable {
 
     */
 
+
     string public name = 'TwoKeyEconomy';
     string public symbol= '2KEY';
     uint8 public decimals= 18;
 
+    address public twoKeyAdmin;
+
+    modifier onlyTwoKeyAdmin {
+        require(msg.sender == twoKeyAdmin);
+        require(address(twoKeyAdmin) != 0);
+    }
+
     constructor (address _twoKeyAdmin) RBACWithAdmin(_twoKeyAdmin) Ownable() public {
         require(_twoKeyAdmin != address(0));
-        TwoKeyAdmin admin = TwoKeyAdmin(_twoKeyAdmin);
+        twoKeyAdmin = _twoKeyAdmin;
         totalSupply_= 1000000000000000000000000;
         balances[_twoKeyAdmin] = totalSupply_;
+    }
+
+    function freezeTransfers() public onlyTwoKeyAdmin{
+        frozen = true;
+    }
+
+    function unfreezeTransfers() public onlyTwoKeyAdmin {
+        frozen = false;
     }
 
 }
