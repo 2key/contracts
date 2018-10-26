489ff1caabff91b481c326b282beb36fbd7ae777
Ehud Ben-Reuven <udi@benreuven.com>
2018-10-26 15:43:52 -0400

    addNameAndPlasmaByUser

--- contracts/2key/TwoKeyReg.sol
+++ contracts/2key/TwoKeyReg.sol
@@ -6,6 +6,11 @@ import './TwoKeyEconomy.sol';
 contract TwoKeyReg is Ownable {
   mapping(address => string) public owner2name;
   mapping(bytes32 => address) public name2owner;
+  // plasma address => ethereum address
+  // note that more than one plasma address can point to the same ethereum address so it is not critical to use the same plasma address all the time for the same user
+  // in some cases the plasma address will be the same as the ethereum address and in that case it is not necessary to have an entry
+  // the way to know if an address is a plasma address is to look it up in this mapping
+  mapping(address => address) public plasma2ethereum;
 
   event UserNameChanged(address owner, string name);
 
@@ -30,6 +35,40 @@ contract TwoKeyReg is Ownable {
 
   function addNameByUser(string _name) public {
     addNameInternal(_name, msg.sender);
+  }
+
+  function addNameAndPlasmaByUser(string _name, bytes sig) public {
+    addNameInternal(_name, msg.sender);
+    bytes32 hash = keccak256(abi.encodePacked(msg.sender));
+    require (sig.length == 65, 'bad signature length');
+    // The signature format is a compact form of:
+    //   {bytes32 r}{bytes32 s}{uint8 v}
+    // Compact means, uint8 is not padded to 32 bytes.
+    uint idx = 32;
+    bytes32 r;
+    assembly
+    {
+      r := mload(add(sig, idx))
+    }
+
+    idx += 32;
+    bytes32 s;
+    assembly
+    {
+      s := mload(add(sig, idx))
+    }
+
+    idx += 1;
+    uint8 v;
+    assembly
+    {
+      v := mload(add(sig, idx))
+    }
+    if (v <= 1) v += 27;
+    require(v==27 || v==28,'bad sig v');
+
+    address plasma_address = ecrecover(hash, v, r, s);
+    plasma2ethereum[plasma_address] = msg.sender;
   }
 
   function getName2Owner(string _name) public view returns (address) {
