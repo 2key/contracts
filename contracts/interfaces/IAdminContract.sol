f0085903d06902fb3c4c7009d1200af10ed56953
madjarevicn <nikola.madjarevic@gmail.com>
2018-10-29 23:40:40 +0100

    Remove indentation

--- contracts/interfaces/IAdminContract.sol
+++ contracts/interfaces/IAdminContract.sol
@@ -1,14 +1,9 @@
 pragma solidity ^0.4.24;
 
 interface AdminContract {
-
     function replaceOneself(address newAdminContract) external;
-
     function transferByAdmins(address to, uint256 tokens) external;
-
     function upgradeEconomyExchangeByAdmins(address newExchange) external;
-
     function transferEtherByAdmins(address to, uint256 amount) external;
-
 }
 
