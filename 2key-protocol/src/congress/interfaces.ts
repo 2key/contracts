2b4ac75436477ff1efeabcb266bae70a77091ce1
madjarevicn <nikola.madjarevic@gmail.com>
2018-10-30 15:38:21 +0100

    Add all methods to interfaces

--- 2key-protocol/src/congress/interfaces.ts
+++ 2key-protocol/src/congress/interfaces.ts
@@ -1,3 +1,11 @@
 export interface ITwoKeyCongress {
     getAllowedMethods: (congress:any, from: string) => Promise<string[]>,
+    isUserMemberOfCongress: (congress:any, member: string, from:string) => Promise<boolean>,
+    submitNewProposal: (congress:any, beneficiary: string, weiAmount: number, jobDescription: string, transactionBytecode: string, from:string) => Promise<number>,
+    newProposalInEther: (congress:any, beneficiary: string, etherAmount: number, jobDescription: string, transactionBytecode: string, from:string) => Promise<number>,
+    getAllProposals: (congress:any, from:string) => Promise<any>,
+    vote: (congress:any, proposalNumber:number, supportsProposal: boolean, justificationText:string, from:string) => Promise<number>,
+    executeProposal: (congress:any, proposalNumber: number, transactionBytecode: string, from: string) => Promise<string>,
+    getVoteCount: (congress:any, proposalNumber: number, from:string) => Promise<any>,
+    getMemberInfo: (congress:any, from: string) => Promise<any>,
 }
