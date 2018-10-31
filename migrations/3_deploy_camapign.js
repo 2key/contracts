80927187515ee33db28e5b82833c30f56be2d0e7
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-10-31 16:34:00 +0200

    SOL Deployed to rinkeby Oct 31, 2018 4:28 PM

--- migrations/3_deploy_camapign.js
+++ migrations/3_deploy_camapign.js
@@ -19,7 +19,7 @@ const ERC20TokenMock = artifacts.require('ERC20TokenMock');
 
 
 module.exports = function deploy(deployer) {
-    if (deployer.network.startsWith('dev') || deployer.network === 'ropsten' || (deployer.network.startsWith('rinkeby') && !process.env.DEPLOY)) {
+    if (deployer.network.startsWith('dev') || deployer.network === 'ropsten' || (deployer.network.startsWith('rinkeby-test') && !process.env.DEPLOY)) {
         deployer.deploy(TwoKeyConversionHandler,1012019,180,6,180)
             .then(() => TwoKeyConversionHandler.deployed())
             .then(() => deployer.deploy(ERC20TokenMock))
