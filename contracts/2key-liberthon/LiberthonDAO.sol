bfc4d5dccf80e1ed2eeaa2b9b27e18a683fdf47d
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-05 09:24:40 +0200

    Initialize Member and Campaign structure

--- contracts/2key-liberthon/LiberthonDAO.sol
+++ contracts/2key-liberthon/LiberthonDAO.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.24;
+
 import "./MemberTypes.sol";
 
 
@@ -10,6 +11,40 @@ contract LiberthonDAO is MemberTypes {
 
     string public organizationName;
     address[] founders;
+    Member[] members;
+    uint numOfMembers;
+
+    mapping(address => Member) addressToMember;
+    mapping(address => MemberType) addressToType;
+    mapping(bytes32 => Member[]) memberTypeToMembers;
+
+    /**
+        Member username, firstname, lastname, and countryOfResidence will be limited to length of 32 ASCII chars
+    */
+    struct Member {
+        bytes32 username;
+        bytes32 firstName;
+        bytes32 lastName;
+        bytes32 countryOfResidence;
+        uint votingPower;
+    }
+
+    struct VotingCampaign {
+        address creator;
+        address[] addressesVoted;
+        string description;
+        uint votesYes;
+        uint votesNo;
+        uint votingPowerYes;
+        uint votingPowerNo;
+    }
+
+
+
+
+
+
+
 
 
 }
