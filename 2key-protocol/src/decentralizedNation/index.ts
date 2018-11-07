cfd69e0813fd3d5cc5258cb5964a586f57185690
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-07 13:03:42 +0200

    Deploy DAO

--- 2key-protocol/src/decentralizedNation/index.ts
+++ 2key-protocol/src/decentralizedNation/index.ts
@@ -1,6 +1,7 @@
 import {ITwoKeyBase, ITwoKeyHelpers} from '../interfaces';
-import {IDecentralizedNation} from "./interfaces";
+import {IDecentralizedNation, IDecentralizedNationConstructor} from "./interfaces";
 import {ITwoKeyUtils} from "../utils/interfaces";
+import {promisify} from "../utils";
 
 export default class DecentralizedNation implements IDecentralizedNation {
     private readonly base: ITwoKeyBase;
@@ -11,6 +12,33 @@ export default class DecentralizedNation implements IDecentralizedNation {
         this.base = twoKeyProtocol;
         this.helpers = helpers;
         this.utils = utils;
+    }
+
+    /**
+     *
+     * @param {IDecentralizedNationConstructor} data
+     * @param {string} from
+     * @returns {Promise<string>}
+     */
+    public createDecentralizedNation(data: IDecentralizedNationConstructor, from: string) : Promise<string> {
+        return new Promise(async(resolve,reject) => {
+            try {
+                const registryAddress = this.base.twoKeyReg.address;
+                const txHash = await promisify(this.helpers._createContract,[
+                    data.nationName,
+                    data.ipfsHashForConstitution,
+                    data.ipfsHashForDAOPublicInfo,
+                    data.initialMemberAddresses,
+                    data.initialMemberTypes,
+                    data.limitsPerMemberType,
+                    this.base.twoKeyReg.address,
+                    { from }
+                ]);
+                resolve(txHash);
+            } catch (e) {
+                reject(e);
+            }
+        })
     }
 
 }
--- 2key-protocol/src/decentralizedNation/interfaces.ts
+++ 2key-protocol/src/decentralizedNation/interfaces.ts
@@ -2,21 +2,13 @@ export interface IDecentralizedNation {
 
 }
 
+
 export interface IDecentralizedNationConstructor {
     nationName: string,
     ipfsHashForConstitution: string,
     ipfsHashForDAOPublicInfo: string,
     initialMemberAddresses: string[],
     initialMemberTypes: string[],
-    twoKeyRegistry: string,
-
+    limitsPerMemberType: number[],
 }
 
-/*
-string _nationName,
-        bytes32 _ipfsHashForConstitution,
-        bytes32 _ipfsHashForDAOPublicInfo,
-        address[] initialMembersAddresses,
-        bytes32[] initialMemberTypes,
-        address _twoKeyRegistry
- */
