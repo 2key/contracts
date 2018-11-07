2b8fcbb1ea7baaa5fa4b6036b619d70647ac90db
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-07 12:41:55 +0200

    UPDATEs

---
+++ 2key-protocol/src/decentralizedNation/index.ts
@@ -0,0 +1,19 @@
+import {ITwoKeyBase, ITwoKeyHelpers} from '../interfaces';
+import {IDecentralizedNation} from "./interfaces";
+import {ITwoKeyUtils} from "../utils/interfaces";
+
+export default class DecentralizedNation implements IDecentralizedNation {
+    private readonly base: ITwoKeyBase;
+    private readonly helpers: ITwoKeyHelpers;
+    private readonly utils: ITwoKeyUtils;
+
+    constructor(twoKeyProtocol: ITwoKeyBase, helpers: ITwoKeyHelpers, utils: ITwoKeyUtils) {
+        this.base = twoKeyProtocol;
+        this.helpers = helpers;
+        this.utils = utils;
+    }
+
+    public
+
+
+}
