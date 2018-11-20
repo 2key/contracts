55412e8416c9353d6a993265bce24fbb24f7d956
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-20 14:00:57 +0100

    export to interfaces

--- 2key-protocol/src/upgradableExchange/interfaces.ts
+++ 2key-protocol/src/upgradableExchange/interfaces.ts
@@ -1,4 +1,7 @@
 export interface IUpgradableExchange {
     getRate: (upgradableExchange: any, from: string) => Promise<number>,
     getERC20Token: (upgradableExchange: any, from: string) => Promise<string>,
+    getAdmin: (upgradableExchange: any, from: string) => Promise<string>,
+    getWeiRaised: (upgradableExchange: any, from: string) => Promise<number>,
+
 }
