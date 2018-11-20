06c16136000367f7b196f74a9997991afcfc2ae4
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-20 13:58:06 +0100

    Add weiRaised to protocol and get admin

--- 2key-protocol/src/upgradableExchange/index.ts
+++ 2key-protocol/src/upgradableExchange/index.ts
@@ -52,4 +52,41 @@ export default class UpgradableExchange implements IUpgradableExchange {
         })
     }
 
+
+    /**
+     *
+     * @param upgradableExchange
+     * @param {string} from
+     * @returns {Promise<string>}
+     */
+    public getAdmin(upgradableExchange: any, from: string) : Promise<string> {
+        return new Promise<string>(async(resolve,reject) => {
+            try {
+                const instance = await this.helpers._getUpgradableExchangeInstance(upgradableExchange);
+                const admin = await promisify(instance.admin,[{from}]);
+                resolve(admin);
+            } catch (e) {
+                reject(e);
+            }
+        })
+    }
+
+    /**
+     *
+     * @param upgradableExchange
+     * @param {string} from
+     * @returns {Promise<number>}
+     */
+    public getWeiRaised(upgradableExchange: any, from: string) : Promise<number> {
+        return new Promise<number>(async(resolve,reject) => {
+            try {
+                const instance = await this.helpers._getUpgradableExchangeInstance(upgradableExchange);
+                const weiRaised = await promisify(instance.weiRaised, [{from}]);
+                resolve(weiRaised);
+            } catch (e) {
+                reject(e);
+            }
+        })
+    }
+
 }
