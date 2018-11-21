85c88f94022f802f6d0597e0c17d739c3da417f8
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-21 12:04:24 +0100

    fetch eth price

---
+++ contracts/2key/TwoKeyFetchETHPrice.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.24;
+
+import "../../installed_contracts/oraclize-api/contracts/usingOraclize.sol";
+
+
+contract TwoKeyFetchETHPrice is usingOraclize {
+    string public ETHUSD;
+    event LogConstructorInitiated(string nextStep);
+    event LogPriceUpdated(string price);
+    event LogNewOraclizeQuery(string description);
+
+    constructor() payable {
+        emit LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Oraclize Query.");
+    }
+
+    function __callback(bytes32 myid, string result) {
+        if (msg.sender != oraclize_cbAddress()) revert();
+        ETHUSD = result;
+        emit LogPriceUpdated(result);
+    }
+
+    function updatePrice() payable {
+        if (oraclize_getPrice("URL") > this.balance) {
+            emit LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+            emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+            oraclize_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price");
+        }
+    }
+}
