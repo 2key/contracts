7510b81756d0367b06e66e8323db5d52d229cd1b
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-22 14:07:56 +0100

    Setup core contracts for upgradability of registry

--- contracts/2key/TwoKeyReg.sol
+++ contracts/2key/TwoKeyReg.sol
@@ -2,66 +2,12 @@ pragma solidity ^0.4.24;
 
 import '../openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import "./RBACWithAdmin.sol";
+import "./TwoKeyRegistryStorage.sol";
 
-contract TwoKeyReg is Ownable {
+contract TwoKeyReg is Ownable, TwoKeyRegistryStorage {
 
     /// @notice Event is emitted when a user's name is changed
     event UserNameChanged(address owner, string name);
-
-    /// mapping user's address to user's name
-    mapping(address => string) public address2username;
-    /// mapping user's name to user's address
-    mapping(bytes32 => address) public username2currentAddress;
-    // mapping username to array of addresses he is using/used
-    mapping(bytes32 => address[]) public username2AddressHistory;
-    /*
-        mapping address to wallet tag
-        wallet tag = username + '_' + walletname
-    */
-    mapping(address => bytes32) address2walletTag;
-
-    // reverse mapping from walletTag to address
-    mapping(bytes32 => address) walletTag2address;
-
-    // plasma address => ethereum address
-    // note that more than one plasma address can point to the same ethereum address so it is not critical to use the same plasma address all the time for the same user
-    // in some cases the plasma address will be the same as the ethereum address and in that case it is not necessary to have an entry
-    // the way to know if an address is a plasma address is to look it up in this mapping
-    mapping(address => address) public plasma2ethereum;
-
-    struct UserData {
-        string username;
-        string fullName;
-        string email;
-    }
-
-    mapping(address => UserData) addressToUserData;
-
-    /*
-        Those mappings are for the fetching data about in what contracts user participates in which role
-    */
-
-    /// mapping users address to addresses of campaigns where he is contractor
-    mapping(address => address[]) userToCampaignsWhereContractor;
-
-    /// mapping users address to addresses of campaigns where he is moderator
-    mapping(address => address[]) userToCampaignsWhereModerator;
-
-    /// mapping users address to addresses of campaigns where he is refferer
-    mapping(address => address[]) userToCampaignsWhereReferrer;
-
-    /// mapping users address to addresses of campaigns where he is converter
-    mapping(address => address[]) userToCampaignsWhereConverter;
-
-    /// Address of 2key event source contract which will have permission to write on this contract
-    /// (Address is enough, there is no need to spend sufficient gas and instantiate whole contract)
-    address public twoKeyEventSource;
-
-    /// Address for contract maintainer
-    /// Should be the array of addresses - will have permission on some of the mappings to update
-    address[] maintainers;
-
-    address twoKeyAdminContractAddress;
 
 
     /// Modifier which will allow only 2key event source to issue calls on selected methods
---
+++ contracts/2key/TwoKeyRegistryProxy.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+import "./TwoKeyRegistryStorage.sol";
+
+contract TwoKeyRegistryProxy is TwoKeyRegistryStorage {
+    address public logic_contract;
+
+    //TODO Add modifiers - ownable or something like that
+    function setLogicContract(address _logicContractAddress) public returns (bool) {
+        logic_contract = _logicContractAddress;
+        return true;
+    }
+
+    //Fallback function
+    function () payable public {
+        address target = logic_contract;
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize)
+            let result := delegatecall(gas, target, ptr, calldatasize, 0, 0)
+            let size := returndatasize
+            returndatacopy(ptr, 0, size)
+            switch result
+            case 0 { revert(ptr, size) }
+            case 1 { return(ptr, size) }
+        }
+    }
+}
