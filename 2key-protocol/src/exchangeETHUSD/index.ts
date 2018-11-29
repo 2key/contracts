74de29494717c0622e939555ba2c2cbb68b1cf0e
madjarevicn <nikola.madjarevic@gmail.com>
2018-11-29 17:33:32 +0100

    Add basic setup for 2key-protocol index&interfaces + add TwoKeyExchangeContract to whitelist deployment

--- 2key-protocol/src/exchangeETHUSD/index.ts
+++ 2key-protocol/src/exchangeETHUSD/index.ts
@@ -1,5 +1,16 @@
 import {IExchangeETHUSD} from "./interfaces";
+import {ITwoKeyBase, ITwoKeyHelpers} from "../interfaces";
+import {ITwoKeyUtils} from "../utils/interfaces";
 
 export default class ExchangeETH implements IExchangeETHUSD {
+    private readonly base: ITwoKeyBase;
+    private readonly helpers: ITwoKeyHelpers;
+    private readonly utils: ITwoKeyUtils;
+
+    constructor(twoKeyProtocol: ITwoKeyBase, helpers: ITwoKeyHelpers, utils: ITwoKeyUtils) {
+        this.base = twoKeyProtocol;
+        this.helpers = helpers;
+        this.utils = utils;
+    }
 
 }
