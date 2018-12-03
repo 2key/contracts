0fbd34da855361e3b5979cd87c498663c28c819c
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-03 13:53:38 +0100

    Add emit before events + getter for proxyAddress for the contract name

--- contracts/2key/TwoKeySingletonesRegistry.sol
+++ contracts/2key/TwoKeySingletonesRegistry.sol
@@ -30,7 +30,7 @@ contract TwoKeySingletonesRegistry is MaintainingPattern, ITwoKeySingletonesRegi
     function addVersion(string contractName, string version, address implementation) public onlyMaintainer {
         require(versions[contractName][version] == 0x0);
         versions[contractName][version] = implementation;
-        VersionAdded(version, implementation);
+        emit VersionAdded(version, implementation);
     }
 
     /**
@@ -43,6 +43,15 @@ contract TwoKeySingletonesRegistry is MaintainingPattern, ITwoKeySingletonesRegi
     }
 
     /**
+     * @notice Function to return address of proxy for specific contract
+     * @param _contractName is the name of the contract we'd like to get proxy address
+     * @return is the address of the proxy for the specific contract
+     */
+    function getContractProxyAddress(string _contractName) public view returns (address) {
+        return contractToProxy[_contractName];
+    }
+
+    /**
     * @dev Creates an upgradeable proxy
     * @param version representing the first version to be set for the proxy
     * @return address of the new proxy created
@@ -51,7 +60,7 @@ contract TwoKeySingletonesRegistry is MaintainingPattern, ITwoKeySingletonesRegi
         UpgradeabilityProxy proxy = new UpgradeabilityProxy(contractName, version);
         Upgradeable(proxy).initialize.value(msg.value)(msg.sender);
         contractToProxy[contractName] = proxy;
-        ProxyCreated(proxy);
+        emit ProxyCreated(proxy);
         return proxy;
     }
 }
