511d22166a15f60513c0146de2dc6bb094f4033c
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-12-06 14:02:37 +0200

    Fix 4_migration

--- migrations/4_deploy_update.js
+++ migrations/4_deploy_update.js
@@ -6,9 +6,9 @@ const Proxy = artifacts.require('UpgradeabilityProxy');
 const json = require('../2key-protocol/src/proxyAddresses.json');
 const fs = require('fs');
 
-let maintainerAddress = (deployer.network.startsWith('ropsten') || deployer.network.startsWith('rinkeby') || deployer.network.startsWith('public.')) ? '0x99663fdaf6d3e983333fb856b5b9c54aa5f27b2f' : '0xbae10c2bdfd4e0e67313d1ebaddaa0adc3eea5d7';
 
 module.exports = function deploy(deployer) {
+    let maintainerAddress = (deployer.network.startsWith('ropsten') || deployer.network.startsWith('rinkeby') || deployer.network.startsWith('public.')) ? '0x99663fdaf6d3e983333fb856b5b9c54aa5f27b2f' : '0xbae10c2bdfd4e0e67313d1ebaddaa0adc3eea5d7';
     console.log(process.argv);
     let found = false;
     process.argv.forEach((argument) => {
