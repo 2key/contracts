601949dba5271699b5f5dfa70c128359557ed566
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-11 14:48:37 +0100

    Delete method from interface which doesn't exist

--- contracts/interfaces/IAdminContract.sol
+++ contracts/interfaces/IAdminContract.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.24;
 interface IAdminContract {
     function replaceOneself(address newAdminContract) external;
     function transferByAdmins(address to, uint256 tokens) external;
-    function upgradeEconomyExchangeByAdmins(address newExchange) external;
     function transferEtherByAdmins(address to, uint256 amount) external;
 }
 
