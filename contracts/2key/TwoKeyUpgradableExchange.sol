dc4d5c2ee570b0ff4051b3a601630adea7f048fc
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-11 13:19:29 +0100

    Delete old deprecated versioning code

--- contracts/2key/TwoKeyUpgradableExchange.sol
+++ contracts/2key/TwoKeyUpgradableExchange.sol
@@ -5,22 +5,13 @@ import '../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
 contract TwoKeyUpgradableExchange is Crowdsale {
 
-	TwoKeyUpgradableExchange filler;
-
 	/// @notice Event is emitted when a user sell his tokens
 	event TokenSell(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-
-	modifier onlyAlive() {
-		require(filler == address(0));
-		_;
-	}
 
 	modifier onlyAdmin() {
 		require(msg.sender == address(admin));
 		_;
 	}
-
 
 	constructor(uint256 _rate, address _twoKeyAdmin, ERC20 _token, address _twoKeyUpgradableExchange)
 		Crowdsale(_rate, _twoKeyAdmin, _token, _twoKeyUpgradableExchange) public {
@@ -36,18 +27,6 @@ contract TwoKeyUpgradableExchange is Crowdsale {
     /// @return Value of tokens in Wei
 	function _getWeiAmount(uint256 _tokenAmount) internal view returns (uint256) {
 	   return _tokenAmount.div(rate);
-	}
-
-	/// @notice Function where only admin can upgrade exchange contract with new exchange contract. 
-    /// @dev This method is called only when alive (i.e. not upgraded to newExchange) and by admin
-    /// @param _to is address of New Exchange Contract
-	function upgrade(address _to) public onlyAlive onlyAdmin {
-		filler = TwoKeyUpgradableExchange(_to);		// check if the address is exchange contract address -- add typecast
-	}
-
-
-	function getFiller() view public returns(address) {
-		return filler;	
 	}
 
 }
