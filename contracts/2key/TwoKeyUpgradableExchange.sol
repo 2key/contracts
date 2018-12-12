97c0576ceb77013a94b078c9e3cabb12f77b6b5f
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-12 14:08:46 +0100

    Add contract code validation layer

--- contracts/2key/TwoKeyUpgradableExchange.sol
+++ contracts/2key/TwoKeyUpgradableExchange.sol
@@ -2,22 +2,44 @@ pragma solidity ^0.4.24;
 
 import '../openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 import '../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import "./GetCode.sol";
 
 contract TwoKeyUpgradableExchange is Crowdsale {
 
-	/// @notice Event is emitted when a user sell his tokens
+	/**
+	 * @notice Event will be fired every time someone buys tokens
+     */
 	event TokenSell(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
-	modifier onlyAdmin() {
-		require(msg.sender == address(admin));
-		_;
-	}
+    /**
+     * @notice Mapping which will map contract bytecode to boolean if that bytecode is eligible to buy tokens
+     */
+    mapping(bytes => bool) isContractEligibleToBuyTokens;
 
-	//TODO: We should somehow add audited contracts here which will be eligible to buyTokens, not everyone
+
+    /**
+     * @notice Modifier which will validate if contract is eligible to buy tokens
+     */
+    modifier onlyEligibleContracts {
+        require(isContractEligibleToBuyTokens[msg.sender] == true);
+        _;
+    }
 
 	constructor(uint256 _rate, address _twoKeyAdmin, ERC20 _token, address _twoKeyUpgradableExchange)
 		Crowdsale(_rate, _twoKeyAdmin, _token, _twoKeyUpgradableExchange) public {
 	}
+
+    /**
+     * @notice Function to add contract code to be eligible to buyTokens
+     * @param _contractAddress is the address of the deployed contract
+     * @dev only maintainer / admin can call this function
+     */
+    function addContractToBeEligibleToBuyTokens(address _contractAddress) public {
+        require(_contractAddress != address(0));
+        bytes memory _contractCode = GetCode.at(_contractAddress);
+        isContractEligibleToBuyTokens[_contractCode] = true;
+    }
+
 
     function () public payable {
         buyTokens(msg.sender);
