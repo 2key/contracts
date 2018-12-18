6d8587d517e4c606cc0b2cc62f4efb4c36b5457a
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-18 15:21:11 +0100

    Add description of the contract

--- contracts/2key/TwoKeyConversionAndConverterStates.sol
+++ contracts/2key/TwoKeyConversionAndConverterStates.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.24;
 
+/**
+ * @notice Contract to store important enumerators
+ * @author Nikola Madjarevic
+ */
 contract TwoKeyConversionAndConverterStates {
     enum ConversionState {PENDING_APPROVAL, APPROVED, EXECUTED, REJECTED, CANCELLED_BY_CONVERTER}
     enum ConverterState {NOT_EXISTING, PENDING_APPROVAL, APPROVED, REJECTED}
