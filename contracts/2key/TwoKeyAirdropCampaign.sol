c5aecd98b8193fa1ee08f65d046ea4cc40b6491c
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-20 17:28:22 +0100

    Add basic setup

--- contracts/2key/TwoKeyAirdropCampaign.sol
+++ contracts/2key/TwoKeyAirdropCampaign.sol
@@ -5,9 +5,23 @@ pragma solidity ^0.4.24;
  * @author Nikola Madjarevic
  * Created at 12/20/18
  */
-
 contract TwoKeyAirdropCampaign {
-    function TwoKeyAirdropCampaign(){
+
+    // This will be the contract ERC20 from which we will do payouts
+    address erc20ContractAddress;
+    // Time when campaign starts
+    uint campaignStartTime;
+    // Time when campaign ends
+    uint campaignEndTime;
+
+    // Modifier which will prevent to do any actions if the time expired or didn't even started yet.
+    modifier isOngoing {
+        require(block.timestamp >= campaignStartTime && block.timestamp <= campaignEndTime);
+        _;
+    }
+
+    constructor() {
 
     }
+
 }
