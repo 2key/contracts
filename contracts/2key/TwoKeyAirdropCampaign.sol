4886090c051aa4fc1551eeab1cdf9e26c240aed6
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-20 17:21:48 +0100

    Create initially AirDrop file campaign

---
+++ contracts/2key/TwoKeyAirdropCampaign.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @notice Contract for the airdrop campaigns
+ * @author Nikola Madjarevic
+ * Created at 12/20/18
+ */
+
+contract TwoKeyAirdropCampaign {
+    function TwoKeyAirdropCampaign(){
+
+    }
+}
