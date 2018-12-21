d04bd56f4cebe0e0149958d29424d848642452e1
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-21 15:16:25 +0100

    Add decode function

--- test/helpers/deserialize.js
+++ test/helpers/deserialize.js
@@ -17,6 +17,32 @@ function deserializeHex(hex) {
     return data;
 }
 
+let testData = "0xca35b7d915458ef540ade6068dfe2f44e8fa733c00000000000000000000000000000000000000000000000000000000000000c8ca35b7d915458ef540ade6068dfe2f44e8fa733c00000000000000000000000000000000000000000000000000000000000030390000000000000000000000000000000000000000000000000000000000bc6143000000000000000000000000000000000000000000000000000000000000000f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006";
+function decode(testData) {
+    let contractor = testData.slice(0,42);
+    let inventoryAmount = parseInt(testData.slice(42, 42+64),16);
+    let assetContractAddress = '0x' + testData.slice(42+64, 42+64+42);
+    let campaignStartTime = parseInt(testData.slice(42+64+40,42+64+40+64),16);
+    let campaignEndTime = parseInt(testData.slice(42+64+40+64, 42+64+40+64+64),16);
+    let numberOfTokensPerConversion = parseInt(testData.slice(42+64+40+64+64,42+64+40+64+64+64),16);
+    let numberOfConversions = parseInt(testData.slice(42+64+40+64+64+64, 42+64+40+64+64+64+64),16);
+    let maxNumberOfConversions = parseInt(testData.slice(42+64+40+64+64+64+64, 42+64+40+64+64+64+64+64),16);
+
+    let obj = {
+        contractor: contractor,
+        inventoryAmount : inventoryAmount,
+        assetContractAddress : assetContractAddress,
+        campaignStartTime : campaignStartTime,
+        campaignEndTime : campaignEndTime,
+        numberOfTokensPerConverter : numberOfTokensPerConversion,
+        numberOfConversions : numberOfConversions,
+        maxNumberOfConversions : maxNumberOfConversions
+    }
+    console.log(obj);
+}
+
+
+
 module.exports = {
-    deserializeHex
+    deserializeHex, decode
 }
