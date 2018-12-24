1022c5241c86aacf2b18506aea23b59e78b34098
madjarevicn <nikola.madjarevic@gmail.com>
2018-12-24 17:32:29 +0100

    Delete comment sufficient

--- 2key-protocol/src/acquisition/index.ts
+++ 2key-protocol/src/acquisition/index.ts
@@ -693,7 +693,6 @@ export default class AcquisitionCampaign implements ITwoKeyAcquisitionCampaign {
                     // const newPublicLink = await this.join(campaignInstance, from, { referralLink: publicLink, cut })
                     const { public_address } = generatePublicMeta();
                     // const signature = Sign.free_join_take(from, public_address, f_address, f_secret, p_message);
-                    // TODO: Nikola try to comment two lines before and uncomment next line 
                     const signature = Sign.free_take(from, f_address, f_secret, p_message);
                     const txHash: string = await promisify(campaignInstance.joinAndConvert, [signature,false, {
                         from,
