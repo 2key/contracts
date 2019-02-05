543ec275073837d2e557f15e2638b65eef821051
madjarevicn <nikola.madjarevic@gmail.com>
2019-02-05 13:39:18 +0100

    add TokenPool contract

---
+++ contracts/2key/TokenPool.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "./Upgradeable.sol";
+import "./MaintainingPattern.sol";
+
+/**
+ * @author Nikola Madjarevic
+ * Created at 2/5/19
+ */
+contract TokenPool is Upgradeable, MaintainingPattern {
+
+}
