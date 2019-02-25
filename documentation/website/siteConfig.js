a4001fca11086d99c76a95b0fafe9145438ae80b
madjarevicn <nikola.madjarevic@gmail.com>
2019-02-25 18:00:51 +0100

    fix-feat/generate-site&fix-header-links

--- documentation/website/siteConfig.js
+++ documentation/website/siteConfig.js
@@ -39,7 +39,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: '2key_ArcERC20', label: 'Docs'},
+    {doc: '2key_DecentralizedNation', label: 'Docs'},
   ],
 
   // If you have users set above, you add it here:
