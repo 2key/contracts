588c10b5c3feab0a5fc0a1fc181bb46f5a3ee641
madjarevicn <nikola.madjarevic@gmail.com>
2019-03-18 10:54:47 +0100

    feat/create-mock-token-fungible

---
+++ contracts/test/FungibleMockToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.24;
+
+import "../2key/singleton-contracts/StandardTokenModified.sol";
+
+/**
+ * @author Nikola Madjarevic
+ * @title Mock token ERC20 which will be used as token sold to improve tests over Acquisition campaigns
+ */
+contract FungibleMockToken is StandardTokenModified {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    constructor (string _name, string _symbol) public {
+        name = _name;
+        symbol = _symbol;
+
+        decimals = 18;
+        totalSupply_= 1000000000000000000000000000; // 1B tokens total minted supply
+        balances[msg.sender]= totalSupply_;
+    }
+
+
+}
