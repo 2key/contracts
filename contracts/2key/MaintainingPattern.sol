a91d63660572ede719917a92be14e67391ab33ec
madjarevicn <nikola.madjarevic@gmail.com>
2019-05-29 02:19:02 +0200

    feat/refactor-maintaining-pattern-to-be-standalone-contract

--- contracts/2key/MaintainingPattern.sol
+++ contracts/2key/MaintainingPattern.sol
@@ -8,6 +8,11 @@ pragma solidity ^0.4.24;
 
 contract MaintainingPattern {
     /**
+     * Flag which will make function setInitialParams callable only once
+     */
+    bool initialized;
+
+    /**
      * Mapping which will store maintainers who are eligible to update contract state
      */
     mapping(address => bool) public isMaintainer;
@@ -18,19 +23,47 @@ contract MaintainingPattern {
     address public twoKeyAdmin;
 
     /**
-     * @notice Modifier to restrict calling the method to anyone but maintainers
+     * @notice Function which can be called only once, and is used as replacement for a constructor
+     * @param _twoKeyAdmin is the address of twoKeyAdmin contract as central authority
+     * @param _maintainers is the array of initial maintainers we'll kick off contract with
      */
-    modifier onlyMaintainer {
+    function setInitialParams(
+        address _twoKeyAdmin,
+        address [] _maintainers
+    )
+    public
+    {
+        require(initialized == false);
+
+        //Set TwoKeyAdmin contract
+        twoKeyAdmin = _twoKeyAdmin;
+
+        //Set deployer to be also a maintainer
+        isMaintainer[msg.sender] = true;
+
+        //Set initial maintainers
+        for(uint i=0; i<_maintainers.length; i++) {
+            isMaintainer[_maintainers[i]] = true;
+        }
+
+        //Once this executes, this function will not be possible to call again.
+        initialized = true;
+    }
+
+    /**
+     * @notice Function to restrict calling the method to anyone but maintainers
+     */
+    function onlyMaintainer() public view returns (bool) {
         require(isMaintainer[msg.sender] == true);
-        _;
+        return true;
     }
 
     /**
      * @notice Modifier to restrict calling the method to anyone but twoKeyAdmin
      */
-    modifier onlyTwoKeyAdmin {
+    function onlyTwoKeyAdmin() public view returns (bool) {
         require(msg.sender == address(twoKeyAdmin));
-        _;
+        return true;
     }
 
     /**
@@ -42,8 +75,8 @@ contract MaintainingPattern {
         address [] _maintainers
     )
     public
-    onlyTwoKeyAdmin
     {
+        require(onlyTwoKeyAdmin() == true);
         //If state variable, .balance, or .length is used several times, holding its value in a local variable is more gas efficient.
         uint numberOfMaintainers = _maintainers.length;
         for(uint i=0; i<numberOfMaintainers; i++) {
@@ -60,8 +93,8 @@ contract MaintainingPattern {
         address [] _maintainers
     )
     public
-    onlyTwoKeyAdmin
     {
+        require(onlyTwoKeyAdmin() == true);
         //If state variable, .balance, or .length is used several times, holding its value in a local variable is more gas efficient.
         uint numberOfMaintainers = _maintainers.length;
         for(uint i=0; i<numberOfMaintainers; i++) {
