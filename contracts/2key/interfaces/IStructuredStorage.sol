96d52234dca87c20255e98d5df699ea8a09a7237
madjarevicn <nikola.madjarevic@gmail.com>
2019-06-10 10:40:22 +0200

    feat/structured-storage-interface

---
+++ contracts/2key/interfaces/IStructuredStorage.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.0;
+
+contract IStructuredStorage {
+    function setProxyLogicContractAndDeployer(address _proxyLogicContract, address _deployer) external;
+
+    // *** Getter Methods ***
+    function getUint(bytes32 _key) external view returns(uint);
+    function getString(bytes32 _key) external view returns(string);
+    function getAddress(bytes32 _key) external view returns(address);
+    function getBytes(bytes32 _key) external view returns(bytes);
+    function getBool(bytes32 _key) external view returns(bool);
+    function getInt(bytes32 _key) external view returns(int);
+
+    // *** Setter Methods ***
+    function setUint(bytes32 _key, uint _value) external;
+    function setString(bytes32 _key, string _value) external;
+    function setAddress(bytes32 _key, address _value) external;
+    function setBytes(bytes32 _key, bytes _value) external;
+    function setBool(bytes32 _key, bool _value) external;
+    function setInt(bytes32 _key, int _value) external;
+
+    // *** Delete Methods ***
+    function deleteUint(bytes32 _key) external;
+    function deleteString(bytes32 _key) external;
+    function deleteAddress(bytes32 _key) external;
+    function deleteBytes(bytes32 _key) external;
+    function deleteBool(bytes32 _key) external;
+    function deleteInt(bytes32 _key) external;
+}
