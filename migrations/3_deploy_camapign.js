dc919e824e90e0f6b32bfb59329881011dd79b10
madjarevicn <nikola.madjarevic@gmail.com>
2019-06-19 12:49:37 +0200

    feat/increase-version-predeployment

--- migrations/3_deploy_camapign.js
+++ migrations/3_deploy_camapign.js
@@ -46,7 +46,7 @@ module.exports = function deploy(deployer) {
             console.log("... Adding implementation versions of Acquisition campaigns");
             await new Promise(async(resolve,reject) => {
                 try {
-                    let version = '1.4';
+                    let version = '1.5';
 
                     let txHash = await TwoKeySingletonesRegistry.at(TwoKeySingletonesRegistry.address)
                         .addVersion('TwoKeyAcquisitionLogicHandler', version, TwoKeyAcquisitionLogicHandler.address);
@@ -70,7 +70,7 @@ module.exports = function deploy(deployer) {
             console.log('... Adding implementation versions of Donation campaigns');
             await new Promise(async(resolve,reject) => {
                 try {
-                    let version = '1.1';
+                    let version = '1.5';
 
                     let txHash = await TwoKeySingletonesRegistry.at(TwoKeySingletonesRegistry.address)
                         .addVersion('TwoKeyDonationCampaign', version, TwoKeyDonationCampaign.address);
