c7cdba8648b87a2bd44ff37d53b16473aa36b657
madjarevicn <nikola.madjarevic@gmail.com>
2019-07-18 13:43:52 +0200

    minor fix 2key-builder

--- 2keyBuilder.js
+++ 2keyBuilder.js
@@ -489,8 +489,10 @@ async function deploy() {
     const l = networks.length;
     for (let i = 0; i < l; i += 1) {
       /* eslint-disable no-await-in-loop */
-      await runProcess(path.join(__dirname, 'node_modules/.bin/truffle'), ['migrate', '--network', networks[i]].concat(process.argv.slice(3)));
-      deployedTo[truffleNetworks[networks[i]].network_id.toString()] = truffleNetworks[networks[i]].network_id;
+      for(let i=4; i<process.argv.length; i++) {
+          await runProcess(path.join(__dirname, 'node_modules/.bin/truffle'), ['migrate', '--network', networks[i]].concat(process.argv[i]));
+          deployedTo[truffleNetworks[networks[i]].network_id.toString()] = truffleNetworks[networks[i]].network_id;
+      }
       await runMigration3(networks[i]);
       /* eslint-enable no-await-in-loop */
     }
