d28f408f79cd9b54f2f6de4e73d009920c21c5e0
Ehud Ben-Reuven <udi@benreuven.com>
2019-07-19 18:19:57 -0400

    POC update plasma and regular registry

--- poc_contracts/2key/depends.sol
+++ poc_contracts/2key/depends.sol
@@ -1,5 +1,22 @@
 import "../../contracts/2key/Migrations.sol";
-import "../../contracts/2key/singleton-contracts/TwoKeyPlasmaEvents.sol";
-import "../../contracts/2key/singleton-contracts/TwoKeyRegistry.sol";
+
+
+import "../../contracts/2key/singleton-contracts/TwoKeyPlasmaSingletoneRegistry.sol";
 import "../../contracts/2key/singleton-contracts/TwoKeySingletonesRegistry.sol";
+
+import "../../contracts/2key/singleton-contracts/TwoKeyPlasmaRegistry.sol";
+import "../../contracts/2key/singleton-storage-contracts/TwoKeyPlasmaRegistryStorage.sol";
+
+import "../../contracts/2key/singleton-contracts/TwoKeyPlasmaEvents.sol";
+import "../../contracts/2key/singleton-storage-contracts/TwoKeyPlasmaEventsStorage.sol";
+
+import "../../contracts/2key/singleton-contracts/TwoKeyMaintainersRegistry.sol";
+import "../../contracts/2key/singleton-storage-contracts/TwoKeyMaintainersRegistryStorage.sol";
+import "../../contracts/2key/singleton-contracts/TwoKeyPlasmaMaintainersRegistry.sol";
+import "../../contracts/2key/singleton-storage-contracts/TwoKeyPlasmaMaintainersRegistryStorage.sol";
+
+import "../../contracts/2key/singleton-contracts/TwoKeySingletonesRegistry.sol";
+import "../../contracts/2key/singleton-contracts/TwoKeyRegistry.sol";
 import "../../contracts/2key/singleton-storage-contracts/TwoKeyRegistryStorage.sol";
+
+import "../../contracts/2key/upgradability/UpgradabilityProxy.sol";
