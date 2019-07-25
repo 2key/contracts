d7872f7e27310915e251bab7288a4749862ccbb3
madjarevicn <nikola.madjarevic@gmail.com>
2019-07-25 14:59:03 +0200

    fix interface

--- contracts/2key/interfaces/ITwoKeyEventSource.sol
+++ contracts/2key/interfaces/ITwoKeyEventSource.sol
@@ -6,4 +6,31 @@ contract ITwoKeyEventSource {
     function plasmaOf(address me) public view returns (address);
     function isAddressMaintainer(address _maintainer) public view returns (bool);
     function getTwoKeyDefaultIntegratorFeeFromAdmin() public view returns (uint);
+    function joined(address _campaign, address _from, address _to) external view;
+    function convertedAcquisitionV2(
+        address _campaign,
+        address _converterPlasma,
+        uint256 _baseTokens,
+        uint256 _bonusTokens,
+        uint256 _conversionAmount,
+        bool _isFiatConversion,
+        uint _conversionId
+    )
+    external
+    view;
+
+    function getTwoKeyDefaultNetworkTaxPercent()
+    public
+    view
+    returns (uint);
+
+    function convertedDonationV2(
+        address _campaign,
+        address _converterPlasma,
+        uint256 _conversionAmount,
+        uint256 _conversionId
+    )
+    external
+    view;
+
 }
