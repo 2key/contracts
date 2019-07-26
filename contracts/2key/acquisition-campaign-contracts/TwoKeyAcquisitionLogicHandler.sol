5ce86f4f69fc49c03047d834047459b44c52191c
phDooY <david91.05@mail.ru>
2019-07-27 01:09:31 +0300

    Unifying function getReferrersBalancesAndTotalEarnings() accross different campaign types

--- contracts/2key/acquisition-campaign-contracts/TwoKeyAcquisitionLogicHandler.sol
+++ contracts/2key/acquisition-campaign-contracts/TwoKeyAcquisitionLogicHandler.sol
@@ -499,6 +499,9 @@ contract TwoKeyAcquisitionLogicHandler is UpgradeableCampaign, TwoKeyCampaignInc
         }
     }
 
+    /**
+     * @notice Internal helper function
+     */
     function recover(
         bytes signature
     )
@@ -726,38 +729,37 @@ contract TwoKeyAcquisitionLogicHandler is UpgradeableCampaign, TwoKeyCampaignInc
     /**
      * @notice Function to fetch for the referrer his balance, his total earnings, and how many conversions he participated in
      * @dev only referrer by himself, moderator, or contractor can call this
-     * @param _referrer is the address of referrer we're checking for
-     * @param signature is the signature if calling functions from FE without ETH address
-     * @param conversionIds are the ids of conversions this referrer participated in
+     * @param _referrerAddress is the address of referrer we're checking for
+     * @param _sig is the signature if calling functions from FE without ETH address
+     * @param _conversionIds are the ids of conversions this referrer participated in
      * @return tuple containing this 3 information
      */
     function getReferrerBalanceAndTotalEarningsAndNumberOfConversions(
-        address _referrer,
-        bytes signature,
-        uint[] conversionIds
+        address _referrerAddress,
+        bytes _sig,
+        uint[] _conversionIds
     )
     public
     view
-    returns (uint,uint,uint,uint[])
+    returns (uint,uint,uint,uint[],address)
     {
-        if(_referrer != address(0)) {
-            require(msg.sender == _referrer || msg.sender == contractor || ITwoKeyMaintainersRegistry(twoKeyMaintainersRegistry).onlyMaintainer(msg.sender));
-            _referrer = plasmaOf(_referrer);
-        } else {
-            bytes32 hash = keccak256(abi.encodePacked(keccak256(abi.encodePacked("bytes binding referrer to plasma")),
-                keccak256(abi.encodePacked("GET_REFERRER_REWARDS"))));
-            _referrer = Call.recoverHash(hash, signature, 0);
+        if(_sig.length > 0) {
+            _referrerAddress = recover(_sig);
+        }
+        else {
+            require(msg.sender == _referrerAddress || msg.sender == contractor || ITwoKeyMaintainersRegistry(twoKeyMaintainersRegistry).onlyMaintainer(msg.sender));
+            _referrerAddress = plasmaOf(_referrerAddress);
         }
 
-        uint len = conversionIds.length;
+        uint len = _conversionIds.length;
         uint[] memory earnings = new uint[](len);
 
         for(uint i=0; i<len; i++) {
-            earnings[i] = referrerPlasma2EarningsPerConversion[_referrer][conversionIds[i]];
+            earnings[i] = referrerPlasma2EarningsPerConversion[_referrerAddress][_conversionIds[i]];
         }
 
-        uint referrerBalance = ITwoKeyAcquisitionCampaignERC20(twoKeyAcquisitionCampaign).getReferrerPlasmaBalance(_referrer);
-        return (referrerBalance, referrerPlasma2TotalEarnings2key[_referrer], referrerPlasmaAddressToCounterOfConversions[_referrer], earnings);
+        uint referrerBalance = ITwoKeyAcquisitionCampaignERC20(twoKeyAcquisitionCampaign).getReferrerPlasmaBalance(_referrerAddress);
+        return (referrerBalance, referrerPlasma2TotalEarnings2key[_referrerAddress], referrerPlasmaAddressToCounterOfConversions[_referrerAddress], earnings, _referrerAddress);
     }
 
 
