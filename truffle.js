666304087ff58e61446fa90c77a4da8d57262793
madjarevicn <nikola.madjarevic@gmail.com>
2019-12-26 10:43:15 +0100

    set production gas price to 8000000000 WEI (8 GWEI)

--- truffle.js
+++ truffle.js
@@ -141,7 +141,7 @@ module.exports = {
           skipDryRun: true,
           network_id: ids["prod-public"],
           gas: 7900000,
-          gasPrice: 120000000000,
+          gasPrice: 8000000000,
       },
 
       'public.prod-hdwallet': {
@@ -149,7 +149,7 @@ module.exports = {
           skipDryRun: true,
           network_id: ids["prod-public"],
           gas: 7900000,
-          gasPrice: 120000000000,
+          gasPrice: 8000000000,
       },
 
       'private.prod-hdwallet': {
