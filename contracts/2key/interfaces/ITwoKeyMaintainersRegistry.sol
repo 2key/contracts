bda34987971c5483c20b6a017256466497a71ceb
madjarevicn <nikola.madjarevic@gmail.com>
2019-12-26 11:36:33 +0100

    dev local gas price

--- contracts/2key/interfaces/ITwoKeyMaintainersRegistry.sol
+++ contracts/2key/interfaces/ITwoKeyMaintainersRegistry.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.24;
 
 contract ITwoKeyMaintainersRegistry {
     function checkIsAddressMaintainer(address _sender) public view returns (bool);
-    function onlyMaintainer(address _sender) public view returns (bool);
-    //TODO: Delete once hard redeploy is done
     function checkIsAddressCoreDev(address _sender) public view returns (bool);
 
     function addMaintainers(address [] _maintainers) public;
     function addCoreDevs(address [] _coreDevs) public;
+    function removeMaintainers(address [] _maintainers) public;
+    function removeCoreDevs(address [] _coreDevs) public;
 }
