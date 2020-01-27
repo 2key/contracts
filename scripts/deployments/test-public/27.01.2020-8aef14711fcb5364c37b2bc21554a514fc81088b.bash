4c64f43e0eb7a25fbe955c868efc572891dd1a68
madjarevicn <nikola.madjarevic@gmail.com>
2020-01-28 00:07:12 +0100

    Add deployment script

---
+++ scripts/deployments/test-public/27.01.2020-8aef14711fcb5364c37b2bc21554a514fc81088b.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+echo "Generating bytecodes for changes on public network"
+echo "Destination for execution: 0xf4797416e6b6835114390591d3ac6a531a061396"
+cd ../..
+
+python3 generate_bytecode.py upgradeContract TwoKeyAdmin 1.0.5
+python3 generate_bytecode.py upgradeContract TwoKeyEventSource 1.0.6
+python3 generate_bytecode.py upgradeContract TwoKeyDeepFreezeTokenPool 1.0.1
+python3 generate_bytecode.py approveNewCampaign DONATION 1.0.12
+python3 generate_bytecode.py approveNewCampaign TOKEN_SELL 1.0.12
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.15
+
+
+
+
+
