1847868e6fafb3225a6fbdc127d5833fb8fe4750
madjarevicn <nikola.madjarevic@gmail.com>
2020-02-03 17:18:35 +0100

    Push bytecode destinations

--- scripts/deployments/test-public/03.02.2020-ab279fec18cd54719f2b98e679cbf2ea23c95e1a.bash
+++ scripts/deployments/test-public/03.02.2020-ab279fec18cd54719f2b98e679cbf2ea23c95e1a.bash
@@ -17,7 +17,14 @@ echo "Destination for execution: 0xf4797416e6b6835114390591d3ac6a531a061396"
 cd ../..
 
 python3 generate_bytecode.py upgradeContract TwoKeyAdmin 1.0.7
-python3 generate_bytecode.py upgradeContract TwoKeyFeeManager 1.0.4
+python3 generate_bytecode.py upgradeContract TwoKeyFeeManager 1.0.6
+python3 generate_bytecode.py upgradeContract TwoKeyEventSource 1.0.8
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.2
+python3 generate_bytecode.py upgradeContract TwoKeyNetworkGrowthFund 1.0.1
+python3 generate_bytecode.py approveNewCampaign DONATION 1.0.18
+python3 generate_bytecode.py approveNewCampaign TOKEN_SELL 1.0.18
+
+
 
 
 
