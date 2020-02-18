f0e168fcb56540c19a9838dda9cb237151ff2e2b
More4ever <More4ever@gmail.com>
2020-02-18 11:17:06 +0200

    Task #3204 - added separate test for TwoKeyExchangeContract (#37)
    * Task #3204 - added separate test for TwoKeyExchangeContract
    - moved related tests from index.spec.ts
    - added missing tests
    - added helper methods for prevent code duplication and make refactor easier in future
    
    * Task #3204 - fixes according to `setValue` method refactoring

---
+++ 2key-protocol/test/helpers/web3Switcher.ts
@@ -0,0 +1,19 @@
+import createWeb3 from "../_web3";
+const { env } = process;
+const rpcUrls = [env.RPC_URL];
+
+//TODO: maybe, should be memoized in future
+const web3Switcher = {
+  deployer: () => createWeb3(env.MNEMONIC_DEPLOYER, rpcUrls),
+  aydnep: () => createWeb3(env.MNEMONIC_AYDNEP, rpcUrls),
+  gmail: () => createWeb3(env.MNEMONIC_GMAIL, rpcUrls),
+  test4: () => createWeb3(env.MNEMONIC_TEST4, rpcUrls),
+  renata: () => createWeb3(env.MNEMONIC_RENATA, rpcUrls),
+  uport: () => createWeb3(env.MNEMONIC_UPORT, rpcUrls),
+  gmail2: () => createWeb3(env.MNEMONIC_GMAIL2, rpcUrls),
+  aydnep2: () => createWeb3(env.MNEMONIC_AYDNEP2, rpcUrls),
+  test: () => createWeb3(env.MNEMONIC_TEST, rpcUrls),
+  guest: () => createWeb3('mnemonic words should be here bu   t for some reason they are missing', rpcUrls),
+};
+
+export default web3Switcher;
