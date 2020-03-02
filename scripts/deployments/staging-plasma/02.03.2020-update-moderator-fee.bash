e01f5f5381e443aaa92d708a949988a4ecdf0907
madjarevicn <nikola.madjarevic@gmail.com>
2020-03-02 12:23:23 +0100

    push bytecodes for execution

---
+++ scripts/deployments/staging-plasma/02.03.2020-update-moderator-fee.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+echo "Destination for execution: 0x52f15890accdf9dac9b98f5afb177d83a4211e08"
+cd ../..
+
+python3 generate_bytecode.py setModeratorFeePlasma 2
+
+
+
+
+
+
