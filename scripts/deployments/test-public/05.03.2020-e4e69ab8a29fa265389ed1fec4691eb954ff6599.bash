022fe5119ddfb809dfcdd74cb019d87b2e4f073b
madjarevicn <nikola.madjarevic@gmail.com>
2020-03-05 16:10:52 +0100

    cleanup

--- scripts/deployments/test-public/05.03.2020-e4e69ab8a29fa265389ed1fec4691eb954ff6599.bash
+++ scripts/deployments/test-public/05.03.2020-e4e69ab8a29fa265389ed1fec4691eb954ff6599.bash
@@ -17,12 +17,9 @@ echo "Generating bytecodes for changes on public network"
 echo "Destination for execution: 0xf4797416e6b6835114390591d3ac6a531a061396"
 cd ../..
 
-python3 generate_bytecode.py approveNewCampaign DONATION 1.0.31
-python3 generate_bytecode.py approveNewCampaign TOKEN_SELL 1.0.31
+python3 generate_bytecode.py approveNewCampaign DONATION 1.0.33
+python3 generate_bytecode.py approveNewCampaign TOKEN_SELL 1.0.33
 
-
-
-
-
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.28
 
 
