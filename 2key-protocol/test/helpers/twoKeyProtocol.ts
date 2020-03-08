78dab69e9fbb07b07bd311f0d3123edad58b04aa
madjarevicn <nikola.madjarevic@gmail.com>
2020-03-08 13:17:33 +0100

    Make twoKeyProtocol use local ipfs also

--- 2key-protocol/test/helpers/twoKeyProtocol.ts
+++ 2key-protocol/test/helpers/twoKeyProtocol.ts
@@ -8,15 +8,21 @@ const networkId: number = Number.parseInt(env.MAIN_NET_ID, 10);
 const privateNetworkId: number = Number.parseInt(env.SYNC_NET_ID, 10);
 const eventsNetUrls: string[] = [env.PLASMA_RPC_URL];
 
+const ipfs = {
+    apiUrl: env.IPFS_URL || 'https://ipfs.2key.net/api/v0'
+};
+
 export const getTwoKeyProtocolValues = (web3, plasmaMnemonic: string): ITwoKeyInit => (
-  {
-    web3,
-    eventsNetUrls,
-    plasmaPK: generatePlasmaFromMnemonic(plasmaMnemonic).privateKey,
-    networkId,
-    privateNetworkId,
-}
+    {
+        web3,
+        ipfs,
+        eventsNetUrls,
+        plasmaPK: generatePlasmaFromMnemonic(plasmaMnemonic).privateKey,
+        networkId,
+        privateNetworkId,
+    }
 );
+
 
 const getTwoKeyProtocol = (web3, plasmaMnemonic: string): TwoKeyProtocol =>  {
   return new TwoKeyProtocol(getTwoKeyProtocolValues(web3, plasmaMnemonic))
