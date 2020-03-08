f69d26d6596bf866f9be0d70ca724565d653490d
madjarevicn <nikola.madjarevic@gmail.com>
2020-03-08 16:39:42 +0100

    Push bytecodes for execution --> 2key-protocol version: 1.4.71-develop

---
+++ scripts/deployments/test-public/08.03.2020-5f17466ec567f9599f5718724d489ff716144c23.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+
+spinner 2
+echo "Generating bytecodes for changes on public network"
+echo "Destination for execution: 0xf4797416e6b6835114390591d3ac6a531a061396"
+cd ../..
+
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.29
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.29
+
+
