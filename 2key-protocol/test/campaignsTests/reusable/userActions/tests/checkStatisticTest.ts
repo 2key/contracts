83b3bef212d505630134787273c074fe40fab8cf
madjarevicn <nikola.madjarevic@gmail.com>
2020-03-25 16:16:08 +0100

    Test funnel remove hack for assertation

--- 2key-protocol/test/campaignsTests/reusable/userActions/tests/checkStatisticTest.ts
+++ 2key-protocol/test/campaignsTests/reusable/userActions/tests/checkStatisticTest.ts
@@ -3,21 +3,6 @@ import functionParamsInterface from "../typings/functionParamsInterface";
 import availableUsers from "../../../../constants/availableUsers";
 import {userStatuses} from "../../../../constants/smallConstants";
 
-/**
- {
-    amountConverterSpentETH: 0,
-    referrerRewards: 1666.6666666666667,
-    tokensBought: 0,
-    isConverter: false,
-    isReferrer: true,
-    isJoined: true,
-    username: 'gmail',
-    fullName: '7YwD8IUQcly0KwM5Jc+IZw==',
-    email: 'RY6B9WJVMQK0tajTtW3jWw==',
-    ethereumOf: '0xf3c7641096bc9dc50d94c572bb455e56efc85412',
-    converterState: 'NOT_CONVERTER'
-  }
- */
 
 export default function checkStatisticTest(
   {
@@ -39,11 +24,11 @@ export default function checkStatisticTest(
     );
     const {isJoined, converterState, tokensBought} = stats;
 
+    console.log('Converter state is: ' + converterState);
+
     expect(converterState).to.be
       .eq(
-        user.status !== userStatuses.rejected // TODO: looks like bug
-          ? user.status
-          : userStatuses.pending
+        user.status
       );
     expect(isJoined).to.be.eq(Boolean(user.link || user.allConversions.length));
     expect(tokensBought).to.be.eq(user.executedConversionsTotal);
