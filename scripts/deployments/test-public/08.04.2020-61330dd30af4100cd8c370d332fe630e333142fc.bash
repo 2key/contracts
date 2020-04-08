d20d3cc4cb6d32915c9118a493bbd05a88b1a999
madjarevicn <nikola.madjarevic@gmail.com>
2020-04-08 10:14:54 +0200

    Push bytecodes for execution

---
+++ scripts/deployments/test-public/08.04.2020-61330dd30af4100cd8c370d332fe630e333142fc.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+
+echo "TwoKeyEventSource new version:                    1.0.11"
+echo "TwoKeyFeeManager new version:                     1.0.11"
+
+
+
+
+echo "Destination for execution: 0xf4797416e6b6835114390591d3ac6a531a061396"
+
+cd ../..
+
+python3 generate_bytecode.py upgradeContract TwoKeyEventSource 1.0.11
+python3 generate_bytecode.py upgradeContract TwoKeyFeeManager 1.0.11
+
+
+
+
