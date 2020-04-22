b7f808fa7ca454b3116d6aa8b42b2c390daf0802
madjarevicn <nikola.madjarevic@gmail.com>
2020-04-22 10:59:41 +0200

    Push script for enable-disable trade

---
+++ scripts/deployments/test-public/22.04.2020-kyber-changes-in-reserve.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+
+echo "Set liquidity params through TwoKeyAdmin contract on Kyber network"
+
+echo "Destination for execution is TwoKeyAdmin address : 0x8430db5eba7745eab1904d173129b7965190055a"
+
+cd ../..
+
+python3 generate_bytecode.py disableKyberTrade 0xC4Fdb3e0399f11959DDD53E356bbA18A2A780AB4
+python3 generate_bytecode.py withdrawEtherFromReserve 0xC4Fdb3e0399f11959DDD53E356bbA18A2A780AB4 69000000000000000000
+python3 generate_bytecode.py setLiquidityParamsKyber 0x3B9140E6a74B7cA28B9dBD1fD3413Ee5aaa1CC2A 10896160231 57437387 40 2000000000000000000 1000000000000000000 30 522389999999999 52239000000000
+python3 generate_bytecode.py enableKyberTrade 0xC4Fdb3e0399f11959DDD53E356bbA18A2A780AB4
