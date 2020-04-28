deaba8f76056f9231adfe1faa121e8daaa08c135
madjarevicn <nikola.madjarevic@gmail.com>
2020-04-28 12:30:51 +0200

    Update script for bytecodes execution

--- scripts/deployments/test-public/28.04.2020-a49aa399b4b6b4e1fcb6942a2e402c38aba85a79.bash
+++ scripts/deployments/test-public/28.04.2020-a49aa399b4b6b4e1fcb6942a2e402c38aba85a79.bash
@@ -14,8 +14,8 @@ spinner() {
 
 spinner 2
 
-echo "TwoKeyAdmin new version:                          1.0.19"
-echo "TwoKeyUpgradableExchange new version:             1.0.9"
+echo "TwoKeyAdmin new version:                          1.0.20"
+echo "TwoKeyUpgradableExchange new version:             1.0.10"
 echo "CPC_PUBLIC campaigns new version:                 1.0.36"
 
 
@@ -24,8 +24,8 @@ echo "Destination for execution: 0xf4797416e6b6835114390591d3ac6a531a061396"
 
 cd ../..
 
-python3 generate_bytecode.py upgradeContract TwoKeyAdmin 1.0.19
-python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.9
+python3 generate_bytecode.py upgradeContract TwoKeyAdmin 1.0.20
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.10
 
 python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.36
 
