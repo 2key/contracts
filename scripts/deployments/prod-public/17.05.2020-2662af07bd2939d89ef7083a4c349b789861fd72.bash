72defa307955511bf16869f1f337ed9a6040fab6
madjarevicn <nikola.madjarevic@gmail.com>
2020-05-17 11:32:14 +0200

    Push bytecode

--- scripts/deployments/prod-public/17.05.2020-2662af07bd2939d89ef7083a4c349b789861fd72.bash
+++ scripts/deployments/prod-public/17.05.2020-2662af07bd2939d89ef7083a4c349b789861fd72.bash
@@ -14,12 +14,14 @@ spinner() {
 spinner 2
 
 
-echo "Destination for execution: 0x60ece4a5be3fd7594e9f24c2948cce5ce3c6dde7"
+echo "Patch for Kyber withdraw tokens"
 
 cd ../..
-
+echo "Destination for execution of Admin patch bytecode: 0x60ece4a5be3fd7594e9f24c2948cce5ce3c6dde7"
 python3 generate_bytecode.py upgradeContract TwoKeyAdmin 1.0.6
 
+echo "Destination for execution is TwoKeyAdmin address : 0x31cf9c7847c979313fe27eadfcc847a8a0252d86"
+python3 generate_bytecode.py withdrawTokensFromReserve 0x00Cd2388C86C960A646D640bE44FC8F83b78cEC9 0xe48972fcd82a274411c01834e2f031d4377fa2c0 10000000000000000000 0x31cf9c7847c979313fe27eadfcc847a8a0252d86
 
 
 
