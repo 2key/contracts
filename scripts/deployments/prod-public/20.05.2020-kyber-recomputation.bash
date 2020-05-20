2bd9e5e70b21fcc6ea4ea5688f5365f4b3fc62f4
madjarevicn <nikola.madjarevic@gmail.com>
2020-05-20 12:51:57 +0200

    Update values

--- scripts/deployments/prod-public/20.05.2020-kyber-recomputation.bash
+++ scripts/deployments/prod-public/20.05.2020-kyber-recomputation.bash
@@ -25,6 +25,8 @@ python3 generate_bytecode.py disableKyberTrade 0x00Cd2388C86C960A646D640bE44FC8F
 echo "--------------------------------------------------------------------------------------------------------------------------------"
 echo "After trade is disabled transfer difference in Ether and Tokens to reserve address: 0x00Cd2388C86C960A646D640bE44FC8F83b78cEC9"
 echo "--------------------------------------------------------------------------------------------------------------------------------"
-python3 generate_bytecode.py setLiquidityParamsKyber 0x063453e3ed9ded626324C2CB9C72e062E4d7089E 9831832975 54205923 40 10000000000000000000 2000000000000000000 35 7394999999999999 49300000000000
+python3 generate_bytecode.py setLiquidityParamsKyber 0x063453e3ed9ded626324C2CB9C72e062E4d7089E 9831832975 57394506 40 10000000000000000000 2000000000000000000 35 7830000000000000 52200000000000
 python3 generate_bytecode.py enableKyberTrade 0x00Cd2388C86C960A646D640bE44FC8F83b78cEC9
+
+
 
