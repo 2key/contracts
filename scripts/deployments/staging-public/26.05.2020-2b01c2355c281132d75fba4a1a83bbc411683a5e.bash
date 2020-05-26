d034e963246df3125ce172a1a4126a260e9a83ab
madjarevicn <nikola.madjarevic@gmail.com>
2020-05-26 12:03:08 +0200

    Push bytecodes for execution

---
+++ scripts/deployments/staging-public/26.05.2020-2b01c2355c281132d75fba4a1a83bbc411683a5e.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+echo "Generating bytecodes for changes on public network"
+echo "Destination for execution: 0x178a57d07d77bd6e2de7236d67a399e2f10c46d9"
+cd ../..
+
+
+python3 generate_bytecode.py upgradeContract TwoKeyAdmin 1.0.8
+python3 generate_bytecode.py upgradeContract TwoKeyEventSource 1.0.7
+python3 generate_bytecode.py upgradeContract TwoKeyFeeManager 1.0.3
+python3 generate_bytecode.py upgradeContract TwoKeyRegistry 1.0.6
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.5
+
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.6
+
+
+
+
