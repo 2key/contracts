7165faf8200f5f1a4cb00201d424c7c538d9901e
madjarevicn <nikola.madjarevic@gmail.com>
2020-06-18 12:15:14 +0200

    Push bytecode for execution

--- scripts/deployments/staging-public/18.06.2020-cfa4cd676232784eebbc967977aad27ecec5ea12.bash
+++ scripts/deployments/staging-public/18.06.2020-cfa4cd676232784eebbc967977aad27ecec5ea12.bash
@@ -19,7 +19,7 @@ echo "Destination for execution: 0x178a57d07d77bd6e2de7236d67a399e2f10c46d9"
 cd ../..
 
 
-python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.8
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.9
 python3 generate_bytecode.py approveNewCampaign TOKEN_SELL 1.0.23
 python3 generate_bytecode.py approveNewCampaign DONATION 1.0.23
 
