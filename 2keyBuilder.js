0b54f4cdd7f4d499235ecac08900d89c3863fa39
madjarevicn <nikola.madjarevic@gmail.com>
2020-07-05 14:06:13 +0200

    Mid deploy

--- 2keyBuilder.js
+++ 2keyBuilder.js
@@ -375,7 +375,9 @@ async function deployUpgrade(networks) {
 
     await runTruffleCompile();
     let [singletonsToBeUpgraded, tokenSellToBePatched, donationToBePatched, cpcChanged] = await getDiffBetweenLatestTags();
-
+    singletonsToBeUpgraded = [] ;
+    tokenSellToBePatched = [];
+    donationToBePatched = [];
     for (let i = 0; i < l; i += 1) {
         /* eslint-disable no-await-in-loop */
         console.log('Singletons to be upgraded: ', singletonsToBeUpgraded);
