cb30cf93daf6f049098a5db69765a16a7fe9659c
madjarevicn <nikola.madjarevic@gmail.com>
2020-07-06 13:43:28 +0200

    Push bytecodes

--- scripts/deployments/test-public/06.07.2020-migrateToKatalyst.bash
+++ scripts/deployments/test-public/06.07.2020-migrateToKatalyst.bash
@@ -14,11 +14,12 @@ spinner() {
 spinner 2
 
 
-echo "Set liquidity params through TwoKeyAdmin contract on Kyber network"
 
-echo "Destination for execution is TwoKeyAdmin address : 0x8430db5eba7745eab1904d173129b7965190055a"
+
+
 
 cd ../..
-
-
-python3 generate_bytecode.py setContracts 0x00Cd2388C86C960A646D640bE44FC8F83b78cEC9 0x65bF64Ff5f51272f729BDcD7AcFB00677ced86Cd 0x063453e3ed9ded626324C2CB9C72e062E4d7089E 0x0000000000000000000000000000000000000000
+echo "Destination for execution is TwoKeySingletonsRegistry address: 0xf4797416e6b6835114390591d3ac6a531a061396"
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.18
+echo "Destination for execution is TwoKeyAdmin address : 0x8430db5eba7745eab1904d173129b7965190055a"
+python3 generate_bytecode.py setContracts 0xC4Fdb3e0399f11959DDD53E356bbA18A2A780AB4 0x920B322D4B8BAB34fb6233646F5c87F87e79952b 0x3b9140e6a74b7ca28b9dbd1fd3413ee5aaa1cc2a 0x0000000000000000000000000000000000000000
