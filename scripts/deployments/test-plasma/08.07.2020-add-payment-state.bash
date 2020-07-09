bf1d9fff963594eb1b97ad589e5133e8bae1b14e
madjarevicn <nikola.madjarevic@gmail.com>
2020-07-09 09:52:42 +0200

    Add bytecode

--- scripts/deployments/test-plasma/08.07.2020-add-payment-state.bash
+++ scripts/deployments/test-plasma/08.07.2020-add-payment-state.bash
@@ -21,5 +21,5 @@ echo "Destination for execution: 0xc96a16b5064797a883f12780400a5e8e5d5d4d20"
 
 cd ../..
 
-python3 generate_bytecode.py approveNewCampaign CPC_PLASMA 1.0.39
+python3 generate_bytecode.py approveNewCampaign CPC_PLASMA 1.0.40
 
