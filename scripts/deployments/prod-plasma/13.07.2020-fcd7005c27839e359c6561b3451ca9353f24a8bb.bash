5c76c2491c03a719941567d234c58fc50b960ac4
madjarevicn <nikola.madjarevic@gmail.com>
2020-07-13 13:46:34 +0200

    Add bytecode for execution.

---
+++ scripts/deployments/prod-plasma/13.07.2020-fcd7005c27839e359c6561b3451ca9353f24a8bb.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+echo "Generating bytecodes for changes on plasma network"
+echo "Destination for execution: 0xd0043ac71897032d572580ad84359323b5719068"
+cd ../..
+
+python3 generate_bytecode.py approveNewCampaign CPC_PLASMA 1.0.5
+
