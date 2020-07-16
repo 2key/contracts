0b71f628d963759a66fbf48deb865809539cad0d
madjarevicn <nikola.madjarevic@gmail.com>
2020-07-16 12:35:08 +0200

    #4316 initialize contract, storage contract, storage contract interface.

---
+++ contracts/2key/singleton-contracts/TwoKeyBudgetCampaignsPaymentsHandler.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+import "../upgradability/Upgradeable.sol";
+import "../non-upgradable-singletons/ITwoKeySingletonUtils.sol";
+
+contract TwoKeyBudgetCampaignsPaymentsHandler is Upgradeable, ITwoKeySingletonUtils {
+
+}
