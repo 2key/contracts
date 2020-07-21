e13be493ecafaf0fdea4125f2ccca75657459885
madjarevicn <nikola.madjarevic@gmail.com>
2020-07-21 14:04:19 +0200

    [#4136] -> Add internal getters and setters on plasma contract.

--- contracts/2key/singleton-contracts/TwoKeyPlasmaBudgetCampaignsPaymentsHandler.sol
+++ contracts/2key/singleton-contracts/TwoKeyPlasmaBudgetCampaignsPaymentsHandler.sol
@@ -1,7 +1,157 @@
 pragma solidity ^0.4.24;
 
 import "../upgradability/Upgradeable.sol";
+import "../interfaces/storage-contracts/ITwoKeyPlasmaBudgetCampaignsPaymentsHandlerStorage.sol";
+
+import "../interfaces/ITwoKeyPlasmaFactory.sol";
+import "../interfaces/ITwoKeySingletoneRegistryFetchAddress.sol";
+import "../interfaces/ITwoKeyMaintainersRegistry.sol";
 
 contract TwoKeyPlasmaBudgetCampaignsPaymentsHandler is Upgradeable {
+
+    bool initialized;
+
+    address public TWO_KEY_PLASMA_SINGLETON_REGISTRY;
+
+    string constant _campaignPlasma2Referrer2earnings = "campaignPlasma2Referrer2rebalancedEarnings";
+    string constant _referrer2TotalEarnings = "referrer2TotalEarnings";
+    string constant _referrer2TotalEarningsPaid = "referrer2TotalEarningsPaid";
+    string constant _referrer2TotalEarningsPending = "referrer2TotalEarningsPending";
+
+
+    ITwoKeyPlasmaBudgetCampaignsPaymentsHandlerStorage public PROXY_STORAGE_CONTRACT;
+
+
+    /**
+     * @notice          Modifier which will be used to restrict calls to only maintainers
+     */
+    modifier onlyMaintainer {
+        require(
+            ITwoKeyMaintainersRegistry(getAddressFromTwoKeySingletonRegistry("TwoKeyPlasmaMaintainersRegistry"))
+            .checkIsAddressMaintainer(msg.sender) == true
+        );
+        _;
+    }
+
+    /**
+     * @notice          Modifier restricting access to the function only to campaigns
+     *                  created using TwoKeyPlasmaFactory contract
+     */
+    modifier onlyBudgetCampaigns {
+        require(
+            ITwoKeyPlasmaFactory(getAddressFromTwoKeySingletonRegistry("TwoKeyPlasmaFactory"))
+            .isCampaignCreatedThroughFactory(msg.sender)
+        );
+        _;
+    }
+
+
+    function setInitialParams(
+        address _twoKeyPlasmaSingletonRegistry,
+        address _proxyStorage
+    )
+    public
+    {
+        require(initialized == false);
+
+        TWO_KEY_PLASMA_SINGLETON_REGISTRY = _twoKeyPlasmaSingletonRegistry;
+        PROXY_STORAGE_CONTRACT = ITwoKeyPlasmaBudgetCampaignsPaymentsHandlerStorage(_proxyStorage);
+
+        initialized = true;
+    }
+
+    /**
+      * ------------------------------------------------
+      *        Internal getters and setters
+      * ------------------------------------------------
+      */
+
+    function getUint(
+        bytes32 key
+    )
+    internal
+    view
+    returns (uint)
+    {
+        return PROXY_STORAGE_CONTRACT.getUint(key);
+    }
+
+    function setUint(
+        bytes32 key,
+        uint value
+    )
+    internal
+    {
+        PROXY_STORAGE_CONTRACT.setUint(key,value);
+    }
+
+    function getBool(
+        bytes32 key
+    )
+    internal
+    view
+    returns (bool)
+    {
+        return PROXY_STORAGE_CONTRACT.getBool(key);
+    }
+
+    function setBool(
+        bytes32 key,
+        bool value
+    )
+    internal
+    {
+        PROXY_STORAGE_CONTRACT.setBool(key,value);
+    }
+
+    function getAddress(
+        bytes32 key
+    )
+    internal
+    view
+    returns (address)
+    {
+        return PROXY_STORAGE_CONTRACT.getAddress(key);
+    }
+
+    function setAddress(
+        bytes32 key,
+        address value
+    )
+    internal
+    {
+        PROXY_STORAGE_CONTRACT.setAddress(key,value);
+    }
+
+    /**
+     * @notice          Function to get address from TwoKeyPlasmaSingletonRegistry
+     *
+     * @param           contractName is the name of the contract
+     */
+    function getAddressFromTwoKeySingletonRegistry(
+        string contractName
+    )
+    internal
+    view
+    returns (address)
+    {
+        return ITwoKeySingletoneRegistryFetchAddress(TWO_KEY_PLASMA_SINGLETON_REGISTRY)
+        .getContractProxyAddress(contractName);
+    }
+
+    // This contract has to store referrers rebalanced rates per campaign
+
+    function setReferrerEarnings(
+        address referrer,
+        uint balance
+    )
+    public
+    onlyBudgetCampaigns
+    {
+        address campaignPlasma = msg.sender;
+
+
+        // TODO: Add modifier only PPC campaigns can call this
+    }
 
 }
