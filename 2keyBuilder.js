e35c6c9e5d9f2c9af7cd176c9661e496db51b66f
madjarevicn <nikola.madjarevic@gmail.com>
2020-08-04 12:30:58 +0200

    Avoid doing tenderly push if contracts are not being deployed.

--- 2keyBuilder.js
+++ 2keyBuilder.js
@@ -549,8 +549,10 @@ async function deploy() {
             process.chdir('../../');
             //Run slack message
             await slack_message('v'+npmVersionTag.toString(), 'v'+oldVersion.toString(), branch_to_env[contractsStatus.current]);
-            // Add tenderly to CI/CD
-            await runProcess('tenderly',['push', '--tag', npmVersionTag]);
+            if(!process.argv.includes('protocol-only')) {
+                // Add tenderly to CI/CD only in case there have been contracts updated.
+                await runProcess('tenderly', ['push', '--tag', npmVersionTag]);
+            }
             // Generate the latest changelog for contracts repo
             await generateChangelog();
             // Go to 2key-protocol/src
