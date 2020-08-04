f49fceb5f213d873690a7491ad0ffce56c76cd05
madjarevicn <nikola.madjarevic@gmail.com>
2020-08-04 12:23:15 +0200

    Add cpcNoRewards to webpack.config.submodules.js

--- webpack.config.submodules.js
+++ webpack.config.submodules.js
@@ -6,7 +6,8 @@ const config = {
   entry: {
     acquisition: './2key-protocol/src/acquisition/index.ts',
     donation: './2key-protocol/src/donation/index.ts',
-    cpc: './2key-protocol/src/cpc/index.ts'
+    cpc: './2key-protocol/src/cpc/index.ts',
+    cpcNoRewards: './2key-protocol/cpcNoRewards/index.ts'
   },
   devtool: 'source-map',
   output: {
