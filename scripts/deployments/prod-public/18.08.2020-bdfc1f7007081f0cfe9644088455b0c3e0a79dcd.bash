658448d9b80c99eb05e222f5aedd8fe956a7915c
madjarevicn <nikola.madjarevic@gmail.com>
2020-08-18 00:35:05 +0200

    Push bytecodes for execution.

---
+++ scripts/deployments/prod-public/18.08.2020-bdfc1f7007081f0cfe9644088455b0c3e0a79dcd.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+
+echo "Destination for execution: 0x60ece4a5be3fd7594e9f24c2948cce5ce3c6dde7"
+
+cd ../..
+
+python3 generate_bytecode.py upgradeContract TwoKeySignatureValidator 1.0.1
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.14
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.6
+
+
+
+
+
