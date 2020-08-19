8231994c0399dbc12ad9c25f26b99667e53eb526
madjarevicn <nikola.madjarevic@gmail.com>
2020-08-19 16:05:09 +0200

    [#4136] -> Add getStableCoinTo2KEYQuota to ITwoKeyExchangeRateContract.sol interface.

--- contracts/2key/interfaces/ITwoKeyExchangeRateContract.sol
+++ contracts/2key/interfaces/ITwoKeyExchangeRateContract.sol
@@ -5,4 +5,5 @@ pragma solidity ^0.4.24;
  */
 contract ITwoKeyExchangeRateContract {
     function getBaseToTargetRate(string _currency) public view returns (uint);
+    function getStableCoinTo2KEYQuota(uint amountStableCoins, address stableCoinAddress) public view returns (uint);
 }
