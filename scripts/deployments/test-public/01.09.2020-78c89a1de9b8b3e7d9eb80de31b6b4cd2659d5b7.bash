73ec709de2d1efe7ec2ab904b279d767cf12bf2b
madjarevicn <nikola.madjarevic@gmail.com>
2020-09-01 14:54:32 +0200

    Push bytecode for execution.

---
+++ scripts/deployments/test-public/01.09.2020-78c89a1de9b8b3e7d9eb80de31b6b4cd2659d5b7.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+echo "Generating bytecodes for changes on public network"
+echo "Destination for execution of contracts patches: 0xf4797416e6b6835114390591d3ac6a531a061396"
+cd ../..
+
+python3 generate_bytecode.py upgradeContract TwoKeyBudgetCampaignsPaymentsHandler 1.0.2
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.27
+python3 generate_bytecode.py approveNewCampaign CPC_PUBLIC 1.0.54
+
