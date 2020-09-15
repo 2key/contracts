4f04f0b8222a2ef800e049474cad5d4ba0f22816
madjarevicn <nikola.madjarevic@gmail.com>
2020-09-15 13:58:17 +0200

    Finish epoch registration.

--- contracts/2key/singleton-contracts/TwoKeyPlasmaParticipationRewards.sol
+++ contracts/2key/singleton-contracts/TwoKeyPlasmaParticipationRewards.sol
@@ -17,6 +17,8 @@ contract TwoKeyPlasmaParticipationRewards is Upgradeable {
     string constant _userToEarningsPerEpoch = "userToEarningsPerEpoch";
     string constant _userToTotalAmountPending = "userToTotalAmountPending";
     string constant _userToTotalAmountWithdrawn = "userToTotalAmountWithdrawn";
+    string constant _userToPendingEpochs = "userToPendingEpochs";
+    string constant _userToWithdrawnEpochs = "userToWithdrawnEpochs";
     string constant _userToSignature = "userToSignature";
     string constant _latestEpochId = "latestEpochId";
     string constant _isEpochIdExisting = "isEpochIdExisting";
@@ -49,69 +51,6 @@ contract TwoKeyPlasmaParticipationRewards is Upgradeable {
         _;
     }
 
-    function registerParticipationMiningEpoch(
-        uint epochId,
-        address [] users,
-        uint [] rewards
-    )
-    public
-    onlyMaintainer
-    {
-        uint totalRewards;
-        // require epoch id doesn't exist
-        require(isEpochIdExisting(epochId) == false);
-        // add this epoch
-        addNewEpoch(epochId);
-
-        uint i;
-
-        for(i = 0; i < users.length; i++) {
-            bytes32 keyUserPendingBalance = keccak256(_userToTotalAmountPending, users[i]);
-            uint userCurrentPendingBalance = getUint(keyUserPendingBalance);
-
-            // Add to user pending balance amount he earned
-            setUint(
-                keyUserPendingBalance,
-                userCurrentPendingBalance.add(rewards[i])
-            );
-
-            // Set user to earnings per epoch
-            setUint(
-                keccak256(_userToEarningsPerEpoch, users[i], epochId),
-                rewards[i]
-            );
-
-        }
-
-        // Emit event for this epoch so on the graph we can do checksums as well
-        ITwoKeyPlasmaEventSource(getAddressFromTwoKeySingletonRegistry("TwoKeyPlasmaEventSource")).emitAddedParticipationMiningEpoch(
-            epochId,
-            users,
-            rewards
-        );
-    }
-
-    function submitSignatureForUserWithdrawal(
-
-    )
-    onlyMaintainer
-    {
-
-    }
-
-
-    function isEpochIdExisting(
-        uint epochId
-    )
-    public
-    view
-    returns (bool)
-    {
-        return getBool(
-            keccak256(_isEpochIdExisting, epochId)
-        );
-    }
-
 
     function addNewEpoch(
         uint epochId
@@ -121,6 +60,19 @@ contract TwoKeyPlasmaParticipationRewards is Upgradeable {
         setBool(
             keccak256(_isEpochIdExisting, epochId),
             true
+        );
+    }
+
+
+    function addEpochToPendingEpochsForUser(
+        address user,
+        uint epochId
+    )
+    internal
+    {
+        appendToUintArray(
+            keccak256(_userToPendingEpochs, user),
+            epochId
         );
     }
 
@@ -165,6 +117,46 @@ contract TwoKeyPlasmaParticipationRewards is Upgradeable {
         PROXY_STORAGE_CONTRACT.setBool(key,value);
     }
 
+    function getUintArray(
+        bytes32 key
+    )
+    internal
+    view
+    returns (uint[])
+    {
+        return PROXY_STORAGE_CONTRACT.getUintArray(key);
+    }
+
+    function appendToUintArray(
+        bytes32 keyOfArray,
+        uint elementToAppend
+    )
+    internal
+    {
+        // Load current array
+        uint [] memory currentArray = getUintArray(keyOfArray);
+
+        uint newArrayLength = currentArray.length+1;
+
+        uint [] memory newArray = new uint[](newArrayLength);
+
+        uint i;
+
+        for(i = 0; i < currentArray.length; i++) {
+            newArray[i] = currentArray[i];
+        }
+
+        // Append element to end of array
+        newArray[i] = elementToAppend;
+
+        // Store new array in storage.
+        PROXY_STORAGE_CONTRACT.setUintArray(
+            keyOfArray,
+            newArray
+        );
+    }
+
+
     /**
      * @notice          Function to get address from TwoKeyPlasmaSingletonRegistry
      *
@@ -179,6 +171,115 @@ contract TwoKeyPlasmaParticipationRewards is Upgradeable {
     {
         return ITwoKeySingletoneRegistryFetchAddress(TWO_KEY_PLASMA_SINGLETON_REGISTRY)
         .getContractProxyAddress(contractName);
+    }
+
+
+    /**
+     * @notice          Function to register new participation mining epoch
+     * @param           epochId is the id of the epoch being registered
+     * @param           users is the array of users earned rewards in this epoch
+     * @param           rewards is the array of rewards users earned in this epoch
+     */
+    function registerParticipationMiningEpoch(
+        uint epochId,
+        address [] users,
+        uint [] rewards
+    )
+    public
+    onlyMaintainer
+    {
+        uint totalRewards;
+        // require epoch id doesn't exist
+        require(isEpochIdExisting(epochId) == false);
+        // add this epoch
+        addNewEpoch(epochId);
+
+        uint i;
+
+        for(i = 0; i < users.length; i++) {
+            bytes32 keyUserPendingBalance = keccak256(_userToTotalAmountPending, users[i]);
+            uint userCurrentPendingBalance = getUint(keyUserPendingBalance);
+
+            // Add to user pending balance amount he earned
+            setUint(
+                keyUserPendingBalance,
+                userCurrentPendingBalance.add(rewards[i])
+            );
+
+            // Set user to earnings per epoch
+            setUint(
+                keccak256(_userToEarningsPerEpoch, users[i], epochId),
+                rewards[i]
+            );
+
+            addEpochToPendingEpochsForUser(users[i], epochId);
+        }
+
+        // Emit event for this epoch so on the graph we can do checksums as well
+        ITwoKeyPlasmaEventSource(getAddressFromTwoKeySingletonRegistry("TwoKeyPlasmaEventSource")).emitAddedParticipationMiningEpoch(
+            epochId,
+            users,
+            rewards
+        );
+    }
+
+
+
+    function submitSignatureForUserWithdrawal(
+
+    )
+    onlyMaintainer
+    {
+
+    }
+
+
+
+    /**
+     * @notice          Function to return if epoch id is already existing
+     * @param           epochId is id of the epoch
+     */
+    function isEpochIdExisting(
+        uint epochId
+    )
+    public
+    view
+    returns (bool)
+    {
+        return getBool(
+            keccak256(_isEpochIdExisting, epochId)
+        );
+    }
+
+    /**
+     * @notice          Function to return statistics per user
+     * @param           user is the user address
+     */
+    function getUserTotalPendingAndWithdrawn(
+        address user
+    )
+    public
+    view
+    returns (uint,uint)
+    {
+        return (
+        getUint(keccak256(_userToTotalAmountPending, user)),
+        getUint(keccak256(_userToTotalAmountWithdrawn, user))
+        );
+    }
+
+    /**
+     * @notice          Function to get pending epochs for user
+     * @param           user is the user address
+     */
+    function getPendingEpochsForUser(
+        address user
+    )
+    public
+    view
+    returns (uint[])
+    {
+        return getUintArray(keccak256(_userToPendingEpochs, user));
     }
 
 
