90e3c4524c7dd73758ba76d51b0efa076f9eec4e
madjarevicn <nikola.madjarevic@gmail.com>
2020-09-16 13:17:16 +0200

    Push bytecodes to execute

--- scripts/deployments/test-plasma/16.09.2020-852df01ebf55033ae44b83299f6e427a0a5116d4.bash
+++ scripts/deployments/test-plasma/16.09.2020-852df01ebf55033ae44b83299f6e427a0a5116d4.bash
@@ -18,4 +18,7 @@ echo "Destination for execution: 0xc96a16b5064797a883f12780400a5e8e5d5d4d20"
 
 cd ../..
 
-python3 generate_bytecode.py upgradeContract TwoKeyPlasmaBudgetCampaignsPaymentsHandler 1.0.2
+python3 generate_bytecode.py upgradeContract TwoKeyPlasmaBudgetCampaignsPaymentsHandler 1.0.3
+python3 generate_bytecode.py upgradeContract TwoKeyPlasmaEventSource 1.0.6
+python3 generate_bytecode.py approveNewCampaign CPC_PLASMA 1.0.56
+
