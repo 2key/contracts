49e19ba9701ea158741c37934620903bb015a33b
madjarevicn <nikola.madjarevic@gmail.com>
2020-09-23 17:07:42 +0200

    Add bash script to unite everything verifying contracts deployment related.

---
+++ scripts/verify-deployment.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "1. Extract latest contracts from 2key-protocol"
+yarn run deploy --extract
+
+echo "--------------------------------------------------------"
+
+start=`date +%s`
+
+echo "2. Run verification script"
+node verifyDeployment.js
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "✨ Done is $runtime seconds. ✨"
