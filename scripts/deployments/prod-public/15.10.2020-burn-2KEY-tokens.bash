2c6f3a485993f8f7300a377b0cfc24942217d7db
madjarevicn <nikola.madjarevic@gmail.com>
2020-10-15 09:56:00 +0200

    Push template with 0x1 and 800k tokens.

--- scripts/deployments/prod-public/15.10.2020-burn-2KEY-tokens.bash
+++ scripts/deployments/prod-public/15.10.2020-burn-2KEY-tokens.bash
@@ -17,5 +17,5 @@ echo "Burn 2KEY tokens on either TwoKeyEconomy contract or https://etherscan.io/
 echo "TwoKeyAdmin contract proxy address on production: 0x31cf9c7847c979313fe27eadfcc847a8a0252d86"
 cd ../..
 
-python3 generate_bytecode.py transfer2KeyTokens <DESTINATION ADDRESS> <AMOUNT_OF_TOKENS_REGULAR_NUMBER_NOT_WEI>
+python3 generate_bytecode.py transfer2KeyTokens 0x0000000000000000000000000000000000000001 800000
 
