864db5d45a5491e0f4eb8a748f7ab3c7c296d884
madjarevicn <nikola.madjarevic@gmail.com>
2020-10-21 10:37:19 +0200

    Fix version.

--- scripts/deployments/test-public/21.10.2020-3bc6266dace4f835c7794182536e35d7459d84cf.bash
+++ scripts/deployments/test-public/21.10.2020-3bc6266dace4f835c7794182536e35d7459d84cf.bash
@@ -16,5 +16,5 @@ echo "Generating bytecodes for changes on public network"
 echo "Destination for execution of contracts patches: 0xf4797416e6b6835114390591d3ac6a531a061396"
 cd ../..
 
-python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.31
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.32
 
