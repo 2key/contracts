495cd4f9e70a545580beb61bf8ff2820ecbcac36
madjarevicn <nikola.madjarevic@gmail.com>
2020-10-25 10:06:45 +0100

    Fix tests.

--- 2key-protocol/test/unitTests/envRelatedTests/transferDAI.spec.ts
+++ 2key-protocol/test/unitTests/envRelatedTests/transferDAI.spec.ts
@@ -22,10 +22,9 @@ describe(
             function () {
                 this.timeout(timeout);
 
-                const {web3, address} = web3Switcher.deployer();
-
+                const {web3, address, plasmaAddress, plasmaWeb3} = web3Switcher.deployer();
                 from = address;
-                twoKeyProtocol = getTwoKeyProtocol(web3, env.MNEMONIC_DEPLOYER);
+                twoKeyProtocol = getTwoKeyProtocol(web3, plasmaWeb3, plasmaAddress);
             }
         );
 
@@ -56,7 +55,7 @@ describe(
                     // Take balance after transfer
                     let balanceAfter = await twoKeyProtocol.ERC20.getERC20Balance(daiAddress, addresses[i]);
                     expect(balanceAfter).to.be.equal(balanceBefore+value);
-                } 
+                }
             }
         }).timeout(timeout);
     }
