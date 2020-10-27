ced97b04df2c1e14641789eec499c1010f63edad
madjarevicn <nikola.madjarevic@gmail.com>
2020-10-27 15:09:20 +0100

    Push bytecodes.

--- scripts/deployments/staging-public/27.10.2020-80b845eb4b3d663a1c4a5b8db75655ea2ab5cadd.bash
+++ scripts/deployments/staging-public/27.10.2020-80b845eb4b3d663a1c4a5b8db75655ea2ab5cadd.bash
@@ -19,7 +19,8 @@ echo "Destination for execution: 0x178a57d07d77bd6e2de7236d67a399e2f10c46d9"
 cd ../..
 
 
-python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.21
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.22
+python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.1
 
 
 
