159a97e4f8b1676390137ce343a878851abcdf0d
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-13 09:52:48 +0100

    Setup singleton on public contract to handle affiliation campaign subscriptions and budget.

---
+++ contracts/2key/singleton-contracts/TwoKeyAffiliationCampaignsPaymentsHandler.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+import "../upgradability/Upgradeable.sol";
+import "../non-upgradable-singletons/ITwoKeySingletonUtils.sol";
+
+contract TwoKeyAffiliationCampaignsPaymentsHandler is Upgradeable, ITwoKeySingletonUtils{
+
+}
