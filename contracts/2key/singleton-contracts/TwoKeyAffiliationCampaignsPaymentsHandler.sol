40c9462db1110e507cb67a82d71c7a04ebdf9df5
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-14 14:08:44 +0100

    Implement function for adding budget, add side functions.

--- contracts/2key/singleton-contracts/TwoKeyAffiliationCampaignsPaymentsHandler.sol
+++ contracts/2key/singleton-contracts/TwoKeyAffiliationCampaignsPaymentsHandler.sol
@@ -17,7 +17,6 @@ contract TwoKeyAffiliationCampaignsPaymentsHandler is Upgradeable, ITwoKeySingle
     string constant _campaignPlasma2SubscriptionTimestamp = "campaignPlasma2SubscriptionDate";
     string constant _campaignPlasma2SubscriptionAmount2KEY = "campaignPlasma2SubscriptionAmount2KEY";
 
-
     /**
      * We need:
      - campaign creation -->
@@ -45,32 +44,76 @@ contract TwoKeyAffiliationCampaignsPaymentsHandler is Upgradeable, ITwoKeySingle
     function addBudgetForCampaign(
         address campaignPlasma,
         address token,
-        uint amount
+        uint amountOfTokens
     )
     public
     {
         if(getCampaignContractor(campaignPlasma) == address(0)) {
             setCampaignContractor(campaignPlasma, msg.sender);
         }
-
+        // Require msg.sender is contractor
         require(msg.sender == getCampaignContractor());
 
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        // Take tokens from contractor
+        IERC20(token).transferFrom(msg.sender, address(this), amountOfTokens);
 
         // 90% of added goes to campaign budget, 10% moderator fee
-        uint campaignBudget = amount.mul(90).div(100);
+        uint campaignBudget = amountOfTokens.mul(90).div(100);
 
         // Compute leftover for moderator
-        uint moderatorLeftover = amount.sub(campaignBudget);
+        uint moderatorLeftover = amountOfTokens.sub(campaignBudget);
+
+        // Increase campaign budget for this campaign
+        increaseCampaignBudget(campaignPlasma,campaignBudget);
 
     }
 
 
-    function addMonthlySubscriptionForCampaign(address campaignPlasma, address token, uint amount) public;
+    function addSubscription2KEY(address campaignPlasma, uint amountOfTokens) public;
+    function addSubscriptionStableCoin(address campaignPlasma, address token, uint amountOfTokens) public;
     function getLatestSubscriptionStartAndEndDate(address campaignPlasma) public view returns (uint,uint);
 
 
 
+    /**
+     * @notice          Function to increase budget for campaign
+     * @param           campaignPlasma is the plasma address of campaign
+     * @param           amountOfTokens is the amount of tokens user is adding to the budget
+     */
+    function increaseCampaignBudget(
+        address campaignPlasma,
+        uint amountOfTokens
+    )
+    internal
+    {
+        uint totalAdded = getTotalAddedBudgetForCampaign(campaignPlasma);
+
+        PROXY_STORAGE_CONTRACT.setUint(
+            keccak256(_campaignPlasma2TotalBudgetAdded, campaignPlasma),
+            totalAdded.add(amountOfTokens)
+        );
+    }
+
+    /**
+     * @notice          Function to get total budget added for the campaign
+     * @param           campaignPlasma is the plasma address of campaign
+     * @return          total budget added for campaign in WEI
+     */
+    function getTotalAddedBudgetForCampaign(
+        address campaignPlasma
+    )
+    public
+    view
+    returns (uint)
+    {
+        return PROXY_STORAGE_CONTRACT.getUint(keccak256(_campaignPlasma2TotalBudgetAdded, campaignPlasma));
+    }
+
+    /**
+     * @notice          Function to get campaign contractor address
+     * @param           campaignPlasma is the address of plasma campaign deployed to sidechain
+     * @return          campaign contractor address
+     */
     function getCampaignContractor(
         address campaignPlasma
     )
@@ -81,7 +124,11 @@ contract TwoKeyAffiliationCampaignsPaymentsHandler is Upgradeable, ITwoKeySingle
         return PROXY_STORAGE_CONTRACT.getAddress(keccak256(_campaignPlasma2Contractor,campaignPlasma));
     }
 
-
+    /**
+     * @notice          Function to set campaign contractor address
+     * @param           campaignPlasma is the address of campaign deployed to sidechain
+     * @param           contractor is contractor address
+     */
     function setCampaignContractor(
         address campaignPlasma,
         address contractor
