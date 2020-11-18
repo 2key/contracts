0386d732b224a1826de27acfa40ba042c0bc2630
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-18 15:03:35 +0100

    Push bytecodes for execution

---
+++ scripts/deployments/test-public/18.11.2020-setSignatoryAddress.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+cd ../..
+
+echo "Destination for execution is TwoKeyParticipationMiningPool address: 0xa6b9ee17f7281f2a6d327ddea2ae812c7439ef36"
+python3 generate_bytecode.py setSignatoryAddress 0xd43f37e636a5b434827ea315b72642e5f00d7bdb
+
