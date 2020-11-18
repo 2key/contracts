0db90605527b09a60edaed752f6299e6ac91aa64
eiTanLaVi <eitan@2key.network>
2020-11-18 17:29:17 +0200

    update executed bytecodes

--- scripts/deployments/test-public/DONE-18.11.2020-setSignatoryAddress.bash
+++ scripts/deployments/test-public/DONE-18.11.2020-setSignatoryAddress.bash
@@ -16,5 +16,6 @@ spinner 2
 cd ../..
 
 echo "Destination for execution is TwoKeyParticipationMiningPool address: 0xa6b9ee17f7281f2a6d327ddea2ae812c7439ef36"
+
 python3 generate_bytecode.py setSignatoryAddress 0xd43f37e636a5b434827ea315b72642e5f00d7bdb
 
