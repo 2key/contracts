27b5c3ee00846920b721898c3cc3ed964fe6a8c2
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-19 10:40:44 +0100

    Set gasPrice to 42 GWEI

--- truffle.js
+++ truffle.js
@@ -133,7 +133,7 @@ module.exports = {
           skipDryRun: true,
           network_id: ids["prod-public"],
           gas: 8000000,
-          gasPrice: 60000000000,
+          gasPrice: 42000000000,
       },
 
       'public.prod-hdwallet': {
