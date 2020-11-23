55e8ae302ea188c4f283708e2e957a4c6b68838c
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-23 10:58:43 +0100

    Push bytecodes for execution.

--- scripts/deployments/test-public/21.11.2020-6eff370b3f8e64affac31735735881a5517d773e.bash
+++ scripts/deployments/test-public/21.11.2020-6eff370b3f8e64affac31735735881a5517d773e.bash
@@ -16,6 +16,10 @@ echo "Generating bytecodes for changes on public network"
 echo "Destination for execution of contracts patches: 0xf4797416e6b6835114390591d3ac6a531a061396"
 cd ../..
 
+
 python3 generate_bytecode.py upgradeContract TwoKeyUpgradableExchange 1.0.36
+python3 generate_bytecode.py upgradeContract TwoKeyParticipationMiningPool 1.0.8
+python3 generate_bytecode.py upgradeContract TwoKeyParticipationPaymentsManager 1.0.1
+
 
 
