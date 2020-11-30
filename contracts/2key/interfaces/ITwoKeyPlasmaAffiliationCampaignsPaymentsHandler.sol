ee42c65ff652fc9118a2609662b63192efbc07a6
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-30 11:32:02 +0100

    Add interface ITwoKeyPlasmaAffiliationCampaignsPaymentsHandler.sol

---
+++ contracts/2key/interfaces/ITwoKeyPlasmaAffiliationCampaignsPaymentsHandler.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+/**
+ * ITwoKeyPlasmaAffiliationCampaignsPaymentsHandler contract.
+ * @author Nikola Madjarevic
+ * Github: madjarevicn
+ */
+contract ITwoKeyPlasmaAffiliationCampaignsPaymentsHandler {
+    function addCampaignToListOfReferrerCampaigns(address referrer) public;
+}
