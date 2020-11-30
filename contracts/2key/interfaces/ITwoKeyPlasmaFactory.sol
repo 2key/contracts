4a82b1c95bfbe6fca1b3def4538bdff473e5d9a1
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-30 10:58:21 +0100

    Expose interface for address2campaignType function.

--- contracts/2key/interfaces/ITwoKeyPlasmaFactory.sol
+++ contracts/2key/interfaces/ITwoKeyPlasmaFactory.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.4.24;
 
 contract ITwoKeyPlasmaFactory {
     function isCampaignCreatedThroughFactory(address _campaignAddress) public view returns (bool);
+    function addressToCampaignType(address _campaignAddress) public view returns (string);
 }
