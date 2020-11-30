7f549583013e6b3c744e3038d42e4577addd5713
madjarevicn <nikola.madjarevic@gmail.com>
2020-11-30 14:53:40 +0100

    Expose getReferrerPlasmaBalance function to interface for affiliation plasma campaign.

---
+++ contracts/2key/interfaces/ITwoKeyPlasmaAffiliationCampaign.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+/**
+ * ITwoKeyPlasmaAffiliationCampaign contract.
+ * @author Nikola Madjarevic
+ * Github: madjarevicn
+ */
+contract ITwoKeyPlasmaAffiliationCampaign {
+    function getReferrerPlasmaBalance(address _referrer) public view returns (uint);
+}
