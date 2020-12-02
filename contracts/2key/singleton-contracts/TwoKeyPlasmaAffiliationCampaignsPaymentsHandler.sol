1a46c1356e4c561a4315a0a8602c761696905e07
madjarevicn <nikola.madjarevic@gmail.com>
2020-12-02 19:58:23 +0100

    Add funnel with pending signature while withdrawal is in process.

--- contracts/2key/singleton-contracts/TwoKeyPlasmaAffiliationCampaignsPaymentsHandler.sol
+++ contracts/2key/singleton-contracts/TwoKeyPlasmaAffiliationCampaignsPaymentsHandler.sol
@@ -28,6 +28,7 @@ contract TwoKeyPlasmaAffiliationCampaignsPaymentsHandler is Upgradeable {
     string constant _referrerToCampaigns = "referrerToCampaigns";
     string constant _referrerToIsCampaignAlreadyAddedToArray = "referrerToIsCampaignAlreadyAddedToArray";
     string constant _isSignatureExisting = "isSignatureExisting";
+    string constant _referrerToPendingSignature = "referrerToPendingSignature";
 
     ITwoKeyPlasmaAffiliationCampaignsPaymentsHandlerStorage public PROXY_STORAGE_CONTRACT;
 
@@ -152,6 +153,41 @@ contract TwoKeyPlasmaAffiliationCampaignsPaymentsHandler is Upgradeable {
         newReferrerCampaigns[i] = campaign;
     }
 
+
+    /**
+     * @notice          Function to set referrer pending signature
+     * @param           referrerPlasma is the address of referrer
+     * @param           signature is signature which is in process of withdrawal.
+     */
+    function setReferrerPendingSignature(
+        address referrerPlasma,
+        bytes signature
+    )
+    internal
+    {
+        PROXY_STORAGE_CONTRACT.setBytes(
+            keccak256(_referrerToPendingSignature, referrerPlasma),
+            signature
+        );
+    }
+
+
+    /**
+     * @notice          Function to remove pending signature stored for referrer
+     * @param           referrerPlasma is referrer plasma address
+     */
+    function removeReferrerPendingSignature(
+        address referrerPlasma
+    )
+    internal
+    {
+        // Remove signature so user can withdraw again once he earns some rewards
+        PROXY_STORAGE_CONTRACT.deleteBytes(
+            keccak256(_referrerToPendingSignature, referrerPlasma)
+        );
+    }
+
+
     /**
      * @notice          Function to get address from TwoKeyPlasmaSingletonRegistry
      *
@@ -246,6 +282,28 @@ contract TwoKeyPlasmaAffiliationCampaignsPaymentsHandler is Upgradeable {
     }
 
 
+    /**
+     * @notice          Function to get referrer pending signature
+     * @param           referrerPlasma is referrer plasma address
+     */
+    function getReferrerPendingSignature(
+        address referrerPlasma
+    )
+    public
+    view
+    returns (bytes)
+    {
+        return PROXY_STORAGE_CONTRACT.getBytes(keccak256(_referrerToPendingSignature, referrerPlasma));
+    }
+
+    /**
+     * @notice          Function to recover signature
+     * @param           signature is the final message received from signing information
+     * @param           referrerPublic is referrer address, and first information being signed
+     * @param           campaigns is the array of campaign addresses from which referrer wants to
+     *                  withdraw his earnings
+     * @param           rewards is the array of rewards for the corresponding campaigns
+     */
     function recoverSignature(
         bytes signature,
         address referrerPublic,
@@ -304,7 +362,14 @@ contract TwoKeyPlasmaAffiliationCampaignsPaymentsHandler is Upgradeable {
         // Require this function to be called only once with same signature
         require(getIfSignatureIsExisting(signature) == false);
 
+        // Get referrer plasma address
         address referrerPlasma = ITwoKeyPlasmaRegistry(getAddressFromTwoKeySingletonRegistry("TwoKeyPlasmaRegistry")).ethereum2plasma(referrerPublicAddress);
+
+        // Get referrer pending signature
+        bytes memory referrerPendingSignature = getReferrerPendingSignature(referrerPlasma);
+
+        // Require that referrer is not having any pending signature
+        require(referrerPendingSignature.length == 0);
         // Require that user is registered
         require(referrerPlasma != address(0));
 
@@ -331,11 +396,9 @@ contract TwoKeyPlasmaAffiliationCampaignsPaymentsHandler is Upgradeable {
 
         // Set that signature exists
         PROXY_STORAGE_CONTRACT.setBool(keccak256(_isSignatureExisting, signature), true);
-        // Withdrawal attepmts per user
-        // [1,2,3,4]
-        // 1 attempt --> signature 1
-        //TODO: Account pending withdrawal
-        // Store signature for withdrawal
+
+        // Store that referrer has pending signature
+        setReferrerPendingSignature(referrerPlasma, signature);
     }
 
     /**
