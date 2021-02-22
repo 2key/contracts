d4498343c5edfe1eea83333dd8548fb4749af79b
madjarevicn <nikola.madjarevic@gmail.com>
2021-02-22 06:54:18 +0100

    [l2-offloading] -> Add withdraw tokens funnel.

--- contracts/2key/interfaces/ITwoKeyRegistry.sol
+++ contracts/2key/interfaces/ITwoKeyRegistry.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.4.24;
 contract ITwoKeyRegistry {
     function checkIfUserExists(address _userAddress) public view returns (bool);
     function getUserData(address _user) public view returns (bytes32,bytes32,bytes32);
+    function getSignatoryAddress() public view returns (address);
 }
