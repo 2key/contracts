fb25bacc624b27eae585e1b436dcc2694a12be77
madjarevicn <nikola.madjarevic@gmail.com>
2021-02-22 10:01:14 +0100

    [l2-offloading] -> Add TwoKeyPlasmaAccountManager.sol contract + interfaces.

--- contracts/2key/singleton-contracts/TwoKeyPlasmaAccountManager.sol
+++ contracts/2key/singleton-contracts/TwoKeyPlasmaAccountManager.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "../upgradability/Upgradeable.sol";
+import "../interfaces/storage-contracts/ITwoKeyPlasmaAccountManagerStorage.sol";
 
 /**
  * TwoKeyPlasmaAccountManager contract.
@@ -8,5 +9,56 @@ import "../upgradability/Upgradeable.sol";
  * Github: madjarevicn
  */
 contract TwoKeyPlasmaAccountManager is Upgradeable {
+    bool initialized;
+
+    string constant _userToUSDTBalance = "userToUSDTBalance";
+    string constant _userTo2KEYBalance = "userTo2KEYBalance";
+
+    address public TWO_KEY_PLASMA_SINGLETON_REGISTRY;
+
+    ITwoKeyPlasmaAccountManagerStorage PROXY_STORAGE_CONTRACT;
+
+    function setInitialParams(
+        address _twoKeyPlasmaSingletonRegistry,
+        address _proxyStorage
+    )
+    public
+    {
+        require(initialized == false);
+
+        TWO_KEY_PLASMA_SINGLETON_REGISTRY = _twoKeyPlasmaSingletonRegistry;
+        PROXY_STORAGE_CONTRACT = ITwoKeyPlasmaAccountManagerStorage(_proxyStorage);
+
+        initialized = true;
+    }
+
+    function addBalanceUSDT(address beneficiary, uint amount) public;
+    function addBalance2KEY(address beneficiary, uint amount) public;
+
+    function get2KEYBalance(address user)
+    public
+    view
+    returns (uint);
+
+    function getUSDTBalance(address user)
+    public
+    view
+    returns (uint);
+
+    function getUSDTDecimals()
+    public
+    view
+    returns (uint)
+    {
+        return 6;
+    }
+
+    function get2KEYDecimals()
+    public
+    view
+    returns (uint)
+    {
+        return 18;
+    }
 
 }
