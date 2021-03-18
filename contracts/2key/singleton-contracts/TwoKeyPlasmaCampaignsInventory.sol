7ee96729d5d3083c8064be8f6f59c9bbabd9fd17
markolazic21 <marko@gmail.com>
2021-03-18 11:15:40 +0100

    [l2-offloading] -> Add 2KEY and USD allocation functions.

--- contracts/2key/singleton-contracts/TwoKeyPlasmaCampaignsInventory.sol
+++ contracts/2key/singleton-contracts/TwoKeyPlasmaCampaignsInventory.sol
@@ -19,6 +19,12 @@ contract TwoKeyPlasmaCampaignsInventory is Upgradeable {
 
     string constant _twoKeyPlasmaMaintainersRegistry = "TwoKeyPlasmaMaintainersRegistry";
 
+    string constant _2KEYBalance = "2KEYBalance";
+    string constant _USDBalance = "USDBalance";
+
+    /**
+     * @notice Function for contract initialization
+     */
     function setInitialParams(
         address _twoKeyPlasmaSingletonRegistry,
         address _proxyStorage
@@ -50,4 +56,36 @@ contract TwoKeyPlasmaCampaignsInventory is Upgradeable {
     function getAddressFromTwoKeySingletonRegistry(string contractName) internal view returns (address) {
         return ITwoKeySingletoneRegistryFetchAddress(TWO_KEY_PLASMA_SINGLETON_REGISTRY).getContractProxyAddress(contractName);
     }
+
+    function allocate2KEY(
+        address user,
+        uint amount
+    )
+    public
+    onlyMaintainer
+    {
+        uint userBalance = PROXY_STORAGE_CONTRACT.getUint(keccak256(_2KEYBalance, user));
+        uint contractBalance = PROXY_STORAGE_CONTRACT.getUint(keccak256(_2KEYBalance, this));
+        userBalance -= amount;
+        contractBalance += amount;
+        PROXY_STORAGE_CONTRACT.setUint(keccak256(_2KEYBalance, user), userBalance);
+        PROXY_STORAGE_CONTRACT.setUint(keccak256(_2KEYBalance, this), contractBalance);
+    }
+
+    function allocateUSD(
+        address user,
+        uint amount
+    )
+    public
+    onlyMaintainer
+    {
+        uint userBalance = PROXY_STORAGE_CONTRACT.getUint(keccak256(_USDBalance, user));
+        uint contractBalance = PROXY_STORAGE_CONTRACT.getUint(keccak256(_USDBalance, this));
+        userBalance -= amount;
+        contractBalance += amount;
+        PROXY_STORAGE_CONTRACT.setUint(keccak256(_USDBalance, user), userBalance);
+        PROXY_STORAGE_CONTRACT.setUint(keccak256(_USDBalance, this), contractBalance);
+    }
+
+
 }
