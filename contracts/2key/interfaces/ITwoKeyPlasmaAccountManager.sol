1d5d3fb847e0b3ced40e9ac886622cb0f75f233b
markolazic21 <M!@mM!@m01>
2021-03-24 14:24:22 +0100

    [l2-offloading] -> Add ITwoKeyPlasmaAccountManager interface and transfer2KEYFrom and transferUSDTFrom functions to PlasmaAccountManager with restriction to be used only by PlasmaCampaignsInventory contract.

---
+++ contracts/2key/interfaces/ITwoKeyPlasmaAccountManager.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.24;
+
+contract ITwoKeyPlasmaAccountManager {
+    function transferUSDTFrom(address user, uint amount) external;
+    function transfer2KEYFrom(address user, uint amount) external;
+}
