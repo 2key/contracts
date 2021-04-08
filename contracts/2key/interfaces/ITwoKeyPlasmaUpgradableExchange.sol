55d160197a0468f90e95134cb5c1dd9d990601d1
markolazic01 <80317369+markolazic01@users.noreply.github.com>
2021-04-08 17:57:09 +0200

    Delete ITwoKeyPlasmaUpgradableExchange.sol

--- contracts/2key/interfaces/ITwoKeyPlasmaUpgradableExchange.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract ITwoKeyPlasmaUpgradableExchange {
-    function sellRate2Key(uint amountToReceive) external view returns (uint);
-    function returnTokensBackToExchange(uint amountOfTokensToReturn) external;
-    function getMore2KeyTokensForRebalancing(uint amountOfTokensRequested) external;
-}
