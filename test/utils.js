a22be5cb395733a33381396836f7e9c6731725f0
0xKey <davidlee152025@gmail.com>
2021-06-15 15:15:51 +1000

    Update test utils for bignumber

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,5 @@
 const truffleAssert = require('truffle-assertions');
-const BigNumber = require("bignumber.js");
+const BigNumber = web3.BigNumber;
 
 async function awaitTx(tx) {
     return await (await tx).wait()
@@ -9,16 +9,9 @@ async function waitForSomeTime(provider, seconds) {
     await provider.send('evm_increaseTime', [seconds])
 }
 
-const decimals = "1000000000000000000"; // 1e18
-
-function toTokenAmountWithDecimals (x) {
-    return new BigNumber(x * decimals);
-}
-
 module.exports = {
     truffleAssert,
     BigNumber,
     awaitTx,
-    waitForSomeTime,
-    toTokenAmountWithDecimals
+    waitForSomeTime
 }
