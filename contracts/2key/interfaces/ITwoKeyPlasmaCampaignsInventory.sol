3b5d8cdf86811a04951b72d203abc09419852e53
0xKey <davidlee152025@gmail.com>
2021-06-17 17:12:36 +1000

    Update interfaces

--- contracts/2key/interfaces/ITwoKeyPlasmaCampaignsInventory.sol
+++ contracts/2key/interfaces/ITwoKeyPlasmaCampaignsInventory.sol
@@ -1,10 +1,45 @@
 pragma solidity ^0.4.24;
 
 contract ITwoKeyPlasmaCampaignsInventory {
+    function addInventory2KEY(
+        uint amount,
+        uint bountyPerConversionUSD,
+        address campaignAddressPlasma
+    )
+    external;
+
+    function addInventoryUSDT(
+        uint amount,
+        uint bountyPerConversionUSD,
+        address campaignAddressPlasma
+    )
+    external;
+
     function endCampaignReserveTokensAndRebalanceRates(
         address campaignPlasma,
         uint totalAmountForReferrerRewards,
         uint totalAmountForModeratorRewards
     )
     external;
+
+    function withdrawLeftoverForContractor(
+        address campaignPlasmaAddress
+    )
+    external;
+
+    function pushAndDistributeRewardsBetweenInfluencers(
+        address [] influencers,
+        uint [] balances,
+        uint nonRebalancedTotalPayout,
+        uint rebalancedTotalPayout,
+        uint cycleId,
+        uint feePerReferrerIn2Key
+    )
+    external;
+
+    function getCampaignInformation(
+        address campaignAddressPlasma
+    )
+    external
+    view;
 }
