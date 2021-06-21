ab09c15fdb4f517846f2801d42f62d4a8ea3641c
0xKey <davidlee152025@gmail.com>
2021-06-21 20:31:06 +1000

    Fix comment and add bignumber package

--- contracts/2key/singleton-contracts/TwoKeyPlasmaCampaignsInventory.sol
+++ contracts/2key/singleton-contracts/TwoKeyPlasmaCampaignsInventory.sol
@@ -99,7 +99,7 @@ contract TwoKeyPlasmaCampaignsInventory is Upgradeable {
     public
     onlyMaintainer
     {
-        // Check if user has already called this function before, if so he can not call it second time
+        // Allow a user add the budget several times but in same token
         require(
             PROXY_STORAGE_CONTRACT.getAddress(keccak256(_campaignPlasma2Contractor, campaignAddressPlasma)) == address(0) || 
             PROXY_STORAGE_CONTRACT.getAddress(keccak256(_campaignPlasma2Contractor, campaignAddressPlasma)) == msg.sender
