ba30267c37e1113227dfb5d327851fb67ca39c5d
0xKey <davidlee152025@gmail.com>
2021-08-04 18:21:59 +1000

    update ITwoKeyPlasmaAccountManager interface

--- contracts/2key/interfaces/ITwoKeyPlasmaAccountManager.sol
+++ contracts/2key/interfaces/ITwoKeyPlasmaAccountManager.sol
@@ -6,4 +6,10 @@ contract ITwoKeyPlasmaAccountManager {
     function transfer2KEY(address beneficiary, uint amount) external;
     function get2KEYBalance(address user) external view returns (uint);
     function getUSDBalance(address user) external view returns (uint);
+    function withdrawModeratorEarningsUSD() external;
+    function withdrawModeratorEarnings2KEY() external;
+    function removeBalanceUSD(address beneficiary, uint amount, bytes signature) external;
+    function removeBalance2JEY(address beneficiary, uint amount, bytes signature) external;
+    function addBalanceUSD(address beneficiary, uint amount, bytes signature) external;
+    function addBalance2KEY(address beneficiary, uint amount, bytes signature) external;
 }
