8e9e238a9e43168133573c373f5d67f6a43ab151
eiTanLaVi <eitan@2key.network>
2021-08-16 10:40:01 +0300

    executed proposals

---
+++ scripts/deployments/prod-plasma/DONE-2.8.2021-declareEpochs43-53.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+spinner() {
+    chars="/-\|"
+
+    for (( j=0; j< $1; j++ )); do
+      for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.5
+        echo -en "${chars:$i:1}" "\r"
+      done
+    done
+}
+
+spinner 2
+
+cd ../..
+
+echo "Destination for execution on plasma is TwoKeyPlasmaParticipationRewards contract: 0xfd5e5de4787b301739e05853ba31dd194e4f180c"
+
+python3 generate_bytecode.py declareEpochs "43,44,45,46,47,48,49,50,51,52,53,54" "88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956,88340000000000145965956"
+
+
+
+
+
